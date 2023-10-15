b889aa3b3930741bc4771b9f078871743439e595
Samuel Laferriere <samlaf92@gmail.com>
2023-10-15 14:17:40 -0700

    Deleted outdated TaskDict struct

--- types/avs.go
+++ types/avs.go
@@ -3,13 +3,6 @@ package types
 import "github.com/Layr-Labs/eigensdk-go/crypto/bls"
 
 type TaskIndex = uint32
-type TaskDict struct {
-	// We only use a single quorum for incredible squaring
-	OperatorsState map[bls.OperatorId]OperatorAvsState
-	// aggregate g1 pubkey of all operators in each quorum, at the blocknumber at which the task was started
-	AggG1PubkeyPerQuorum map[QuorumNum]*bls.G1Point
-}
-
 type TaskResponseDigest [32]byte
 
 type SignedTaskResponseDigest struct {
