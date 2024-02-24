52c50b741847ac6fc2478d61b5ca280c84e9b7f3
Samuel Laferriere <samlaf92@gmail.com>
2024-02-24 10:51:17 -0700

    broke up constructor so we can create text or json sloggers, and added code for proper source info logging in dev mode

--- logging/slog_logger.go
+++ logging/slog_logger.go
@@ -5,6 +5,15 @@ import (
 	"fmt"
 	"log/slog"
 	"os"
+	"runtime"
+	"time"
+)
+
+type SlogHandlerType string
+
+const (
+	SLogHandlerTypeJson     SlogHandlerType = "json" // prints debug and above
+	SLogHandlerTypeJsonText SlogHandlerType = "text" // prints info and above
 )
 
 type SLogger struct {
@@ -13,66 +22,101 @@ type SLogger struct {
 
 var _ Logger = (*SLogger)(nil)
 
-func NewSlogLogger(env LogLevel) Logger {
+func NewSlogTextLogger(env LogLevel) Logger {
+	var levelHandler *LevelHandler
 	if env == Production {
-		jsonHandler := slog.NewJSONHandler(os.Stdout, nil).WithAttrs([]slog.Attr{slog.String("env", string(env))})
-		levelHandler := NewLevelHandler(slog.LevelInfo, jsonHandler) // production logs only info and above
-		logger := slog.New(levelHandler)
-		return &SLogger{
-			logger: logger,
-		}
+		textHandler := slog.NewTextHandler(os.Stdout, nil).WithAttrs([]slog.Attr{slog.String("env", string(env))})
+		levelHandler = NewLevelHandler(slog.LevelInfo, textHandler) // production logs only info and above
 	} else if env == Development {
-		jsonHandler := slog.NewJSONHandler(os.Stdout, nil).WithAttrs([]slog.Attr{slog.String("env", string(env))})
-		levelHandler := NewLevelHandler(slog.LevelDebug, jsonHandler) // development logs everything
-		logger := slog.New(levelHandler)
-		return &SLogger{
-			logger: logger,
-		}
+		// we add source information in development mode only
+		textHandler := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{AddSource: true}).WithAttrs([]slog.Attr{slog.String("env", string(env))})
+		levelHandler = NewLevelHandler(slog.LevelDebug, textHandler) // development logs everything
 	} else {
 		panic(fmt.Sprintf("Unknown environment. Expected %s or %s. Received %s.", Development, Production, env))
+	}
+	logger := slog.New(levelHandler)
+	return &SLogger{
+		logger: logger,
+	}
+}
+
+func NewSlogJsonLogger(env LogLevel) Logger {
+	var levelHandler *LevelHandler
+	if env == Production {
+		jsonHandler := slog.NewJSONHandler(os.Stdout, nil).WithAttrs([]slog.Attr{slog.String("env", string(env))})
+		levelHandler = NewLevelHandler(slog.LevelInfo, jsonHandler) // production logs only info and above
+	} else if env == Development {
+		// we add source information in development mode only
+		jsonHandler := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{AddSource: true}).WithAttrs([]slog.Attr{slog.String("env", string(env))})
+		levelHandler = NewLevelHandler(slog.LevelDebug, jsonHandler) // development logs everything
+	} else {
+		panic(fmt.Sprintf("Unknown environment. Expected %s or %s. Received %s.", Development, Production, env))
+	}
+	logger := slog.New(levelHandler)
+	return &SLogger{
+		logger: logger,
 	}
 }
 
 func (s SLogger) Debug(msg string, tags ...any) {
-	s.logger.Debug(msg, tags...)
+	s.logCorrectSource(slog.LevelDebug, msg, tags...)
 }
 
 func (s SLogger) Info(msg string, tags ...any) {
-	s.logger.Info(msg, tags...)
+	s.logCorrectSource(slog.LevelInfo, msg, tags...)
 }
 
 func (s SLogger) Warn(msg string, tags ...any) {
-	s.logger.Warn(msg, tags...)
+	s.logCorrectSource(slog.LevelWarn, msg, tags...)
 }
 
 func (s SLogger) Error(msg string, tags ...any) {
-	s.logger.Error(msg, tags...)
+	s.logCorrectSource(slog.LevelError, msg, tags...)
 }
 
 func (s SLogger) Fatal(msg string, tags ...any) {
-	s.logger.Error(msg, tags...)
+	s.logCorrectSource(slog.LevelError, msg, tags...)
 	os.Exit(1)
+}
+
+// logCorrectSource logs the message with the correct source information
+// adapted from https://pkg.go.dev/log/slog#example-package-Wrapping
+func (s SLogger) logCorrectSource(level slog.Level, msg string, tags ...any) {
+	var pcs [1]uintptr
+	runtime.Callers(3, pcs[:]) // skip [Callers, logCorrectSource, Info/Debug/Warn/Error/Fatal]
+	r := slog.NewRecord(time.Now(), level, msg, pcs[0])
+	r.Add(tags...)
+	_ = s.logger.Handler().Handle(context.Background(), r)
 }
 
 func (s SLogger) Debugf(template string, args ...interface{}) {
-	s.logger.Debug(fmt.Sprintf(template, args...))
+	s.logfCorrectSource(slog.LevelDebug, template, args...)
 }
 
 func (s SLogger) Infof(template string, args ...interface{}) {
-	s.logger.Info(fmt.Sprintf(template, args...))
+	s.logfCorrectSource(slog.LevelInfo, template, args...)
 }
 
 func (s SLogger) Warnf(template string, args ...interface{}) {
-	s.logger.Warn(fmt.Sprintf(template, args...))
+	s.logfCorrectSource(slog.LevelWarn, template, args...)
 }
 
 func (s SLogger) Errorf(template string, args ...interface{}) {
-	s.logger.Error(fmt.Sprintf(template, args...))
+	s.logfCorrectSource(slog.LevelError, template, args...)
 }
 
 func (s SLogger) Fatalf(template string, args ...interface{}) {
-	s.logger.Error(fmt.Sprintf(template, args...))
+	s.logfCorrectSource(slog.LevelError, template, args...)
 	os.Exit(1)
+}
+
+// logfCorrectSource logs the message with the correct source information
+// adapted from https://pkg.go.dev/log/slog#example-package-Wrapping
+func (s SLogger) logfCorrectSource(level slog.Level, template string, args ...interface{}) {
+	var pcs [1]uintptr
+	runtime.Callers(3, pcs[:]) // skip [Callers, logfCorrectSource, Info/Debug/Warn/Error/Fatal]
+	r := slog.NewRecord(time.Now(), level, fmt.Sprintf(template, args...), pcs[0])
+	_ = s.logger.Handler().Handle(context.Background(), r)
 }
 
 // Taken from https://pkg.go.dev/log/slog@master#example-Handler-LevelHandler
