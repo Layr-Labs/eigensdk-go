df441e7ad9bb8b835ef3295e8b86c5be4cfbb7c6
Samuel Laferriere <samlaf92@gmail.com>
2024-02-24 15:39:10 -0700

    implement much simpler version of having log level

--- logging/slog_logger.go
+++ logging/slog_logger.go
@@ -23,36 +23,44 @@ type SLogger struct {
 var _ Logger = (*SLogger)(nil)
 
 func NewSlogTextLogger(env LogLevel) Logger {
-	var levelHandler *LevelHandler
+	var handler slog.Handler
 	if env == Production {
-		textHandler := slog.NewTextHandler(os.Stdout, nil).WithAttrs([]slog.Attr{slog.String("env", string(env))})
-		levelHandler = NewLevelHandler(slog.LevelInfo, textHandler) // production logs only info and above
+		handler = slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+			Level: slog.LevelInfo, // prints info and above (default behavior but we add to be explicit)
+		}).
+			WithAttrs([]slog.Attr{slog.String("env", string(env))}) // add a default tag with env name
 	} else if env == Development {
-		// we add source information in development mode only
-		textHandler := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{AddSource: true}).WithAttrs([]slog.Attr{slog.String("env", string(env))})
-		levelHandler = NewLevelHandler(slog.LevelDebug, textHandler) // development logs everything
+		handler = slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+			AddSource: true,            // we add source information in development mode only
+			Level:     slog.LevelDebug, // prints debug and above
+		}).
+			WithAttrs([]slog.Attr{slog.String("env", string(env))}) // add a default tag with env name
 	} else {
 		panic(fmt.Sprintf("Unknown environment. Expected %s or %s. Received %s.", Development, Production, env))
 	}
-	logger := slog.New(levelHandler)
+	logger := slog.New(handler)
 	return &SLogger{
 		logger: logger,
 	}
 }
 
 func NewSlogJsonLogger(env LogLevel) Logger {
-	var levelHandler *LevelHandler
+	var handler slog.Handler
 	if env == Production {
-		jsonHandler := slog.NewJSONHandler(os.Stdout, nil).WithAttrs([]slog.Attr{slog.String("env", string(env))})
-		levelHandler = NewLevelHandler(slog.LevelInfo, jsonHandler) // production logs only info and above
+		handler = slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+			Level: slog.LevelInfo, // prints info and above (default behavior but we add to be explicit)
+		}).
+			WithAttrs([]slog.Attr{slog.String("env", string(env))}) // add a default tag with env name
 	} else if env == Development {
-		// we add source information in development mode only
-		jsonHandler := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{AddSource: true}).WithAttrs([]slog.Attr{slog.String("env", string(env))})
-		levelHandler = NewLevelHandler(slog.LevelDebug, jsonHandler) // development logs everything
+		handler = slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+			AddSource: true,            // we add source information in development mode only
+			Level:     slog.LevelDebug, // prints debug and above
+		}).
+			WithAttrs([]slog.Attr{slog.String("env", string(env))}) // add a default tag with env name
 	} else {
 		panic(fmt.Sprintf("Unknown environment. Expected %s or %s. Received %s.", Development, Production, env))
 	}
-	logger := slog.New(levelHandler)
+	logger := slog.New(handler)
 	return &SLogger{
 		logger: logger,
 	}
@@ -117,49 +125,4 @@ func (s SLogger) logfCorrectSource(level slog.Level, template string, args ...in
 	runtime.Callers(3, pcs[:]) // skip [Callers, logfCorrectSource, Info/Debug/Warn/Error/Fatal]
 	r := slog.NewRecord(time.Now(), level, fmt.Sprintf(template, args...), pcs[0])
 	_ = s.logger.Handler().Handle(context.Background(), r)
-}
-
-// Taken from https://pkg.go.dev/log/slog@master#example-Handler-LevelHandler
-// A LevelHandler wraps a Handler with an Enabled method
-// that returns false for levels below a minimum.
-// This permits our logger to only log messages of a certain level or higher
-type LevelHandler struct {
-	level   slog.Leveler
-	handler slog.Handler
-}
-
-// NewLevelHandler returns a LevelHandler with the given level.
-// All methods except Enabled delegate to h.
-func NewLevelHandler(level slog.Leveler, h slog.Handler) *LevelHandler {
-	// Optimization: avoid chains of LevelHandlers.
-	if lh, ok := h.(*LevelHandler); ok {
-		h = lh.Handler()
-	}
-	return &LevelHandler{level, h}
-}
-
-// Enabled implements Handler.Enabled by reporting whether
-// level is at least as large as h's level.
-func (h *LevelHandler) Enabled(_ context.Context, level slog.Level) bool {
-	return level >= h.level.Level()
-}
-
-// Handle implements Handler.Handle.
-func (h *LevelHandler) Handle(ctx context.Context, r slog.Record) error {
-	return h.handler.Handle(ctx, r)
-}
-
-// WithAttrs implements Handler.WithAttrs.
-func (h *LevelHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
-	return NewLevelHandler(h.level, h.handler.WithAttrs(attrs))
-}
-
-// WithGroup implements Handler.WithGroup.
-func (h *LevelHandler) WithGroup(name string) slog.Handler {
-	return NewLevelHandler(h.level, h.handler.WithGroup(name))
-}
-
-// Handler returns the Handler wrapped by h.
-func (h *LevelHandler) Handler() slog.Handler {
-	return h.handler
 }
