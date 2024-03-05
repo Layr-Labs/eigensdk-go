36a80cc83d6e8a4ea511533675faac192c53f210
Ian Shim <100327837+ian-shim@users.noreply.github.com>
2024-03-04 17:26:34 -0800

    [HotWallet] Handle Transaction Replacement at Fireblock Wallet (#134)
    * replace tx
    
    * add comment

--- chainio/clients/wallet/fireblocks_wallet_test.go
+++ chainio/clients/wallet/fireblocks_wallet_test.go
@@ -176,6 +176,97 @@ func TestSendTransactionInvalidVault(t *testing.T) {
 	assert.Equal(t, "", txID)
 }
 
+func TestSendTransactionReplaceTx(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	fireblocksClient := cmocks.NewMockFireblocksClient(ctrl)
+	ethClient := mocks.NewMockEthClient(ctrl)
+	logger, err := logging.NewZapLogger(logging.Development)
+	assert.NoError(t, err)
+	ethClient.EXPECT().ChainID(gomock.Any()).Return(big.NewInt(5), nil)
+	sender, err := wallet.NewFireblocksWallet(fireblocksClient, ethClient, vaultAccountName, logger)
+	assert.NoError(t, err)
+
+	fireblocksClient.EXPECT().ListContracts(gomock.Any()).Return([]fireblocks.WhitelistedContract{
+		{
+			ID:   "contractID",
+			Name: "TestContract",
+			Assets: []struct {
+				ID      fireblocks.AssetID `json:"id"`
+				Status  string             `json:"status"`
+				Address common.Address     `json:"address"`
+				Tag     string             `json:"tag"`
+			}{{
+				ID:      "ETH_TEST3",
+				Status:  "ACTIVE",
+				Address: common.HexToAddress(contractAddress),
+				Tag:     "",
+			},
+			},
+		},
+	}, nil)
+	fireblocksClient.EXPECT().ContractCall(gomock.Any(), gomock.Any()).Return(&fireblocks.ContractCallResponse{
+		ID:     "1234",
+		Status: fireblocks.Confirming,
+	}, nil)
+	fireblocksClient.EXPECT().ListVaultAccounts(gomock.Any()).Return([]fireblocks.VaultAccount{
+		{
+			ID:   "vaultAccountID",
+			Name: vaultAccountName,
+			Assets: []fireblocks.Asset{
+				{
+					ID:        "ETH_TEST3",
+					Total:     "1",
+					Balance:   "1",
+					Available: "1",
+				},
+			},
+		},
+	}, nil)
+
+	txID, err := sender.SendTransaction(context.Background(), types.NewTransaction(
+		0,                                    // nonce
+		common.HexToAddress(contractAddress), // to
+		big.NewInt(0),                        // value
+		100000,                               // gas
+		big.NewInt(100),                      // gasPrice
+		common.Hex2Bytes("0x6057361d00000000000000000000000000000000000000000000000000000000000f4240"), // data
+	))
+	assert.NoError(t, err)
+	assert.Equal(t, "1234", txID)
+
+	newTx := types.NewTransaction(
+		0,                                    // nonce
+		common.HexToAddress(contractAddress), // to
+		big.NewInt(0),                        // value
+		100000,                               // gas
+		big.NewInt(100),                      // gasPrice
+		common.Hex2Bytes("0x6057361d00000000000000000000000000000000000000000000000000000000000f4240"), // data
+	)
+	expectedTxHash := "0xdeadbeef"
+	fireblocksClient.EXPECT().GetTransaction(gomock.Any(), "1234").Return(&fireblocks.Transaction{
+		ID:     expectedTxHash,
+		Status: fireblocks.Completed,
+		TxHash: expectedTxHash,
+	}, nil)
+	fireblocksClient.EXPECT().ContractCall(gomock.Any(), fireblocks.NewContractCallRequest(
+		newTx.Hash().Hex(),
+		"ETH_TEST3",
+		"vaultAccountID",
+		"contractID",
+		"0",
+		"0x",
+		expectedTxHash,
+	)).Return(&fireblocks.ContractCallResponse{
+		ID:     "5678",
+		Status: fireblocks.Confirming,
+	}, nil)
+	// send another tx with the same nonce
+	txID, err = sender.SendTransaction(context.Background(), newTx)
+	assert.NoError(t, err)
+	assert.Equal(t, "5678", txID)
+}
+
 func TestWaitForTransactionReceipt(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
