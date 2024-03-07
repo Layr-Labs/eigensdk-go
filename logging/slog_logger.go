01719ce9868b4082d0d4cf26d71d08afbea3275e
Samuel Laferriere <samlaf92@gmail.com>
2024-03-06 17:00:48 -0700

    Updates to slog to make it the default feature-full logger (#127)
    * added level handler to slog
    
    * broke up constructor so we can create text or json sloggers, and added code for proper source info logging in dev mode
    
    * implement much simpler version of having log level
    
    * make the slog logger an embedded struct (instead of named field) so that we have access to its method
    also return SLogger struct from constructors
    
    * make slog constructors more flexible (more arguments)
    
    * remove no longer needed SlogHandlerType
    
    * correct logLevel comment
    
    * pass full handlerOpts as parameter to give user max flexibility

--- logging/slog_logger.go
+++ logging/slog_logger.go
@@ -1,75 +1,108 @@
 package logging
 
 import (
+	"context"
 	"fmt"
+	"io"
 	"log/slog"
 	"os"
+	"runtime"
+	"time"
 )
 
 type SLogger struct {
-	logger *slog.Logger
+	*slog.Logger
 }
 
 var _ Logger = (*SLogger)(nil)
 
-func NewSlogLogger(env LogLevel) Logger {
-	if env == Production {
-		logHandler := slog.NewJSONHandler(os.Stdout, nil).WithAttrs([]slog.Attr{slog.String("env", string(env))})
-		logger := slog.New(logHandler)
-		return &SLogger{
-			logger: logger,
-		}
-	} else if env == Development {
-		// Even though code is same for both environments, we are keeping it separate to show that we can have different
-		// implementations for different environments. For example, we can have log levels set differently
-		logHandler := slog.NewJSONHandler(os.Stdout, nil).WithAttrs([]slog.Attr{slog.String("env", string(env))})
-		logger := slog.New(logHandler)
-		return &SLogger{
-			logger: logger,
-		}
-	} else {
-		panic(fmt.Sprintf("Unknown environment. Expected %s or %s. Received %s.", Development, Production, env))
+// NewSlogTextLogger creates a new SLogger with a text handler
+//
+//		outputWriter is the writer to write the logs to (typically os.Stdout,
+//		  but can also use a io.MultiWriter(os.Stdout, file) to write to multiple outputs)
+//	 	handlerOptions is the options for the handler, such as
+//		Level is the minimum level to log
+//		AddSource if true, adds source information to the log
+func NewSlogTextLogger(outputWriter io.Writer, handlerOpts *slog.HandlerOptions) *SLogger {
+	handler := slog.NewTextHandler(outputWriter, handlerOpts)
+	logger := slog.New(handler)
+	return &SLogger{
+		logger,
+	}
+}
+
+// NewSlogJsonLogger creates a new SLogger with a Json handler
+//
+//		outputWriter is the writer to write the logs to (typically os.Stdout,
+//		  but can also use a io.MultiWriter(os.Stdout, file) to write to multiple outputs)
+//	 	handlerOptions is the options for the handler, such as
+//		Level is the minimum level to log
+//		AddSource if true, adds source information to the log
+func NewSlogJsonLogger(outputWriter io.Writer, handlerOpts *slog.HandlerOptions) *SLogger {
+	handler := slog.NewJSONHandler(outputWriter, handlerOpts)
+	logger := slog.New(handler)
+	return &SLogger{
+		logger,
 	}
 }
 
 func (s SLogger) Debug(msg string, tags ...any) {
-	s.logger.Debug(msg, tags...)
+	s.logCorrectSource(slog.LevelDebug, msg, tags...)
 }
 
 func (s SLogger) Info(msg string, tags ...any) {
-	s.logger.Info(msg, tags...)
+	s.logCorrectSource(slog.LevelInfo, msg, tags...)
 }
 
 func (s SLogger) Warn(msg string, tags ...any) {
-	s.logger.Warn(msg, tags...)
+	s.logCorrectSource(slog.LevelWarn, msg, tags...)
 }
 
 func (s SLogger) Error(msg string, tags ...any) {
-	s.logger.Error(msg, tags...)
+	s.logCorrectSource(slog.LevelError, msg, tags...)
 }
 
 func (s SLogger) Fatal(msg string, tags ...any) {
-	s.logger.Error(msg, tags...)
+	s.logCorrectSource(slog.LevelError, msg, tags...)
 	os.Exit(1)
+}
+
+// logCorrectSource logs the message with the correct source information
+// adapted from https://pkg.go.dev/log/slog#example-package-Wrapping
+func (s SLogger) logCorrectSource(level slog.Level, msg string, tags ...any) {
+	var pcs [1]uintptr
+	runtime.Callers(3, pcs[:]) // skip [Callers, logCorrectSource, Info/Debug/Warn/Error/Fatal]
+	r := slog.NewRecord(time.Now(), level, msg, pcs[0])
+	r.Add(tags...)
+	_ = s.Handler().Handle(context.Background(), r)
 }
 
 func (s SLogger) Debugf(template string, args ...interface{}) {
-	s.logger.Debug(fmt.Sprintf(template, args...))
+	s.logfCorrectSource(slog.LevelDebug, template, args...)
 }
 
 func (s SLogger) Infof(template string, args ...interface{}) {
-	s.logger.Info(fmt.Sprintf(template, args...))
+	s.logfCorrectSource(slog.LevelInfo, template, args...)
 }
 
 func (s SLogger) Warnf(template string, args ...interface{}) {
-	s.logger.Warn(fmt.Sprintf(template, args...))
+	s.logfCorrectSource(slog.LevelWarn, template, args...)
 }
 
 func (s SLogger) Errorf(template string, args ...interface{}) {
-	s.logger.Error(fmt.Sprintf(template, args...))
+	s.logfCorrectSource(slog.LevelError, template, args...)
 }
 
 func (s SLogger) Fatalf(template string, args ...interface{}) {
-	s.logger.Error(fmt.Sprintf(template, args...))
+	s.logfCorrectSource(slog.LevelError, template, args...)
 	os.Exit(1)
+}
+
+// logfCorrectSource logs the message with the correct source information
+// adapted from https://pkg.go.dev/log/slog#example-package-Wrapping
+func (s SLogger) logfCorrectSource(level slog.Level, template string, args ...interface{}) {
+	var pcs [1]uintptr
+	runtime.Callers(3, pcs[:]) // skip [Callers, logfCorrectSource, Info/Debug/Warn/Error/Fatal]
+	r := slog.NewRecord(time.Now(), level, fmt.Sprintf(template, args...), pcs[0])
+	_ = s.Handler().Handle(context.Background(), r)
 }
