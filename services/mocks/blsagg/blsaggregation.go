e509ccd28e614338833a40c45054ce55bbf49c72
Samuel Laferriere <samlaf92@gmail.com>
2024-05-30 15:46:43 +0800

    make mocks

--- services/mocks/blsagg/blsaggregation.go
+++ services/mocks/blsagg/blsaggregation.go
@@ -72,7 +72,7 @@ func (mr *MockBlsAggregationServiceMockRecorder) InitializeNewTask(arg0, arg1, a
 }
 
 // ProcessNewSignature mocks base method.
-func (m *MockBlsAggregationService) ProcessNewSignature(arg0 context.Context, arg1 uint32, arg2 types.Bytes32, arg3 *bls.Signature, arg4 types.Bytes32) error {
+func (m *MockBlsAggregationService) ProcessNewSignature(arg0 context.Context, arg1 uint32, arg2 any, arg3 *bls.Signature, arg4 types.Bytes32) error {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "ProcessNewSignature", arg0, arg1, arg2, arg3, arg4)
 	ret0, _ := ret[0].(error)
