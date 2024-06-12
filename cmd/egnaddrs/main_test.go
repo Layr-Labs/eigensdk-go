783d5e6d8939773d73ab854d877eec087c6ed9c7
Samuel Laferriere <samlaf92@gmail.com>
2024-06-12 15:15:48 +0800

    update StartAnvilContainer + make egnaddrs test use new deployed anvil db json file

--- cmd/egnaddrs/main_test.go
+++ cmd/egnaddrs/main_test.go
@@ -8,11 +8,7 @@ import (
 )
 
 const (
-	// just copied this file from an eigencert deployment and hardcoded addrs below
-	// TODO(samlaf): eventually we should make updating this file automated
-	anvilStateFileName      = "eigenlayer-and-registries-deployed-anvil-state.json"
-	serviceManagerAddr      = "0x7a2088a1bFc9d81c55368AE168C2C02570cB814F"
-	registryCoordinatorAddr = "0x09635F643e140090A9A8Dcd712eD6285858ceBef"
+	anvilStateFileName = "contracts-deployed-anvil-state.json" // in contracts/anvil/
 )
 
 func TestEgnAddrsWithServiceManagerFlag(t *testing.T) {
@@ -21,14 +17,15 @@ func TestEgnAddrsWithServiceManagerFlag(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	anvilEndpoint, err := anvilC.Endpoint(context.Background(), "")
+	anvilEndpoint, err := anvilC.Endpoint(context.Background(), "http")
 	if err != nil {
 		t.Error(err)
 	}
+	contractAddrs := testutils.GetContractAddressesFromContractRegistry(anvilEndpoint)
 
 	args := []string{"egnaddrs"}
-	args = append(args, "--service-manager", serviceManagerAddr)
-	args = append(args, "--rpc-url", "http://"+anvilEndpoint)
+	args = append(args, "--service-manager", contractAddrs.ServiceManager.Hex())
+	args = append(args, "--rpc-url", anvilEndpoint)
 	// we just make sure it doesn't crash
 	run(args)
 }
@@ -39,14 +36,15 @@ func TestEgnAddrsWithRegistryCoordinatorFlag(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	anvilEndpoint, err := anvilC.Endpoint(context.Background(), "")
+	anvilEndpoint, err := anvilC.Endpoint(context.Background(), "http")
 	if err != nil {
 		t.Error(err)
 	}
+	contractAddrs := testutils.GetContractAddressesFromContractRegistry(anvilEndpoint)
 
 	args := []string{"egnaddrs"}
-	args = append(args, "--registry-coordinator", registryCoordinatorAddr)
-	args = append(args, "--rpc-url", "http://"+anvilEndpoint)
+	args = append(args, "--registry-coordinator", contractAddrs.RegistryCoordinator.Hex())
+	args = append(args, "--rpc-url", anvilEndpoint)
 	// we just make sure it doesn't crash
 	run(args)
 }
--- testutils/anvil.go
+++ testutils/anvil.go
@@ -3,18 +3,19 @@ package testutils
 import (
 	"context"
 	"fmt"
-	"os"
 	"path/filepath"
+	"runtime"
 
+	"github.com/Layr-Labs/eigensdk-go/chainio/clients/eth"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/wait"
+
+	contractreg "github.com/Layr-Labs/eigensdk-go/contracts/bindings/ContractsRegistry"
 )
 
 func StartAnvilContainer(anvilStateFileName string) (testcontainers.Container, error) {
-	integrationDir, err := os.Getwd()
-	if err != nil {
-		panic(err)
-	}
 
 	ctx := context.Background()
 	req := testcontainers.ContainerRequest{
@@ -28,9 +29,10 @@ func StartAnvilContainer(anvilStateFileName string) (testcontainers.Container, e
 	if anvilStateFileName != "" {
 		fmt.Println("Starting Anvil container with state file: ", anvilStateFileName)
 		req.Cmd = append(req.Cmd, "--load-state", "/root/.anvil/state.json")
+		_, curFilePath, _, _ := runtime.Caller(0)
 		req.Files = []testcontainers.ContainerFile{
 			{
-				HostFilePath:      filepath.Join(integrationDir, "test_data", anvilStateFileName),
+				HostFilePath:      filepath.Join(curFilePath, "../../contracts/anvil/", anvilStateFileName),
 				ContainerFilePath: "/root/.anvil/state.json",
 				FileMode:          0644, // Adjust the FileMode according to your requirements
 			},
@@ -40,4 +42,69 @@ func StartAnvilContainer(anvilStateFileName string) (testcontainers.Container, e
 		ContainerRequest: req,
 		Started:          true,
 	})
+}
+
+type ContractAddresses struct {
+	ServiceManager         common.Address
+	RegistryCoordinator    common.Address
+	OperatorStateRetriever common.Address
+	DelegationManager      common.Address
+	Erc20MockStrategy      common.Address
+}
+
+func GetContractAddressesFromContractRegistry(ethHttpUrl string) (mockAvsContracts ContractAddresses) {
+	ethHttpClient, err := eth.NewClient(ethHttpUrl)
+	if err != nil {
+		panic(err)
+	}
+	// The ContractsRegistry contract should always be deployed at this address on anvil
+	// it's the address of the contract created at nonce 0 by the first anvil account (0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266)
+	contractsRegistry, err := contractreg.NewContractContractsRegistry(common.HexToAddress("0x5FbDB2315678afecb367f032d93F642f64180aa3"), ethHttpClient)
+	if err != nil {
+		panic(err)
+	}
+
+	mockAvsServiceManagerAddr, err := contractsRegistry.Contracts(&bind.CallOpts{}, "mockAvsServiceManager")
+	if err != nil {
+		panic(err)
+	}
+	if mockAvsServiceManagerAddr == (common.Address{}) {
+		panic("mockAvsServiceManagerAddr is empty")
+	}
+	mockAvsRegistryCoordinatorAddr, err := contractsRegistry.Contracts(&bind.CallOpts{}, "mockAvsRegistryCoordinator")
+	if err != nil {
+		panic(err)
+	}
+	if mockAvsRegistryCoordinatorAddr == (common.Address{}) {
+		panic("mockAvsRegistryCoordinatorAddr is empty")
+	}
+	mockAvsOperatorStateRetrieverAddr, err := contractsRegistry.Contracts(&bind.CallOpts{}, "mockAvsOperatorStateRetriever")
+	if err != nil {
+		panic(err)
+	}
+	if mockAvsOperatorStateRetrieverAddr == (common.Address{}) {
+		panic("mockAvsOperatorStateRetrieverAddr is empty")
+	}
+	delegationManagerAddr, err := contractsRegistry.Contracts(&bind.CallOpts{}, "delegationManager")
+	if err != nil {
+		panic(err)
+	}
+	if delegationManagerAddr == (common.Address{}) {
+		panic("delegationManagerAddr is empty")
+	}
+	erc20MockStrategyAddr, err := contractsRegistry.Contracts(&bind.CallOpts{}, "erc20MockStrategy")
+	if err != nil {
+		panic(err)
+	}
+	if erc20MockStrategyAddr == (common.Address{}) {
+		panic("erc20MockStrategyAddr is empty")
+	}
+	mockAvsContracts = ContractAddresses{
+		ServiceManager:         mockAvsServiceManagerAddr,
+		RegistryCoordinator:    mockAvsRegistryCoordinatorAddr,
+		OperatorStateRetriever: mockAvsOperatorStateRetrieverAddr,
+		DelegationManager:      delegationManagerAddr,
+		Erc20MockStrategy:      erc20MockStrategyAddr,
+	}
+	return mockAvsContracts
 }
