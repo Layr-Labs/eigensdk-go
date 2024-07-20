2dd4f40ac1f2129009e76b6b5f54f420b37f88c3
Madhur Shrimal <madhur.shrimal@gmail.com>
2024-07-20 14:53:29 -0700

    remove event mock

--- chainio/gen.go
+++ chainio/gen.go
@@ -1,6 +1,5 @@
 package chainio
 
 //go:generate mockgen -destination=./mocks/ethclient.go -package=mocks -mock_names=Client=MockEthClient github.com/Layr-Labs/eigensdk-go/chainio/clients/eth Client
-//go:generate mockgen -destination=./mocks/eventSubscription.go -package=mocks github.com/ethereum/go-ethereum/event Subscription
 //go:generate mockgen -destination=./clients/mocks/fireblocks.go -package=mocks -mock_names=Client=MockFireblocksClient github.com/Layr-Labs/eigensdk-go/chainio/clients/fireblocks Client
 //go:generate mockgen -destination=./clients/mocks/wallet.go -package=mocks github.com/Layr-Labs/eigensdk-go/chainio/clients/wallet Wallet
