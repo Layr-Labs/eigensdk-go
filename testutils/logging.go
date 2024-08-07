38dfa870cec30f0374c9dba4ec9661bf55b88317
Samuel Laferriere <samlaf92@gmail.com>
2024-08-07 14:05:26 -0700

    make fmt

--- testutils/logging.go
+++ testutils/logging.go
@@ -11,5 +11,8 @@ import (
 // It returns an slog textHandler logger that outputs to os.Stdout, with source code information and debug level
 func NewTestLogger() logging.Logger {
 	// we don't use colors because the test output panel in vscode doesn't support them
-	return logging.NewTextSLogger(os.Stdout, &logging.SLoggerOptions{AddSource: true, Level: slog.LevelDebug, NoColor: true})
+	return logging.NewTextSLogger(
+		os.Stdout,
+		&logging.SLoggerOptions{AddSource: true, Level: slog.LevelDebug, NoColor: true},
+	)
 }
