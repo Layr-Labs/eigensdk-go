62be544e1570d70dda9c70c11f212f3066b3533e
Pablo Deymonnaz <deymonnaz@gmail.com>
2024-09-24 17:19:14 -0300

    fmt

--- chainio/clients/avsregistry/writer.go
+++ chainio/clients/avsregistry/writer.go
@@ -349,7 +349,9 @@ func (w *ChainWriter) RegisterOperator(
 		return nil, err
 	}
 	sigValidForSeconds := int64(60 * 60) // 1 hour
-	operatorToAvsRegistrationSigExpiry := new(big.Int).Add(new(big.Int).SetUint64(curBlock.Time()), big.NewInt(sigValidForSeconds))
+	operatorToAvsRegistrationSigExpiry := new(
+		big.Int,
+	).Add(new(big.Int).SetUint64(curBlock.Time()), big.NewInt(sigValidForSeconds))
 
 	// params to register operator in delegation manager's operator-avs mapping
 	msgToSign, err := w.elReader.CalculateOperatorAVSRegistrationDigestHash(
