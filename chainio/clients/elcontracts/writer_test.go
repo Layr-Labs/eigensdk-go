70a83aa00845c1835463f20c7f839457a9460dde
tomasarrachea <tomas.arrachea@lambdaclass.com>
2024-10-02 15:56:59 -0300

    separate register operator tests

--- chainio/clients/elcontracts/writer_test.go
+++ chainio/clients/elcontracts/writer_test.go
@@ -3,6 +3,7 @@ package elcontracts_test
 import (
 	"context"
 	"log/slog"
+	"math/big"
 	"os"
 	"testing"
 
@@ -16,6 +17,98 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestRegisterOperator(t *testing.T) {
+	anvilStateFileName := "contracts-deployed-anvil-state.json"
+	anvilC, err := testutils.StartAnvilContainer(anvilStateFileName)
+	require.NoError(t, err)
+	anvilHttpEndpoint, err := anvilC.Endpoint(context.Background(), "http")
+	require.NoError(t, err)
+	anvilWsEndpoint, err := anvilC.Endpoint(context.Background(), "ws")
+	require.NoError(t, err)
+	logger := logging.NewTextSLogger(os.Stdout, &logging.SLoggerOptions{Level: slog.LevelDebug})
+
+	contractAddrs := testutils.GetContractAddressesFromContractRegistry(anvilHttpEndpoint)
+	require.NoError(t, err)
+
+	chainioConfig := clients.BuildAllConfig{
+		EthHttpUrl:                 anvilHttpEndpoint,
+		EthWsUrl:                   anvilWsEndpoint,
+		RegistryCoordinatorAddr:    contractAddrs.RegistryCoordinator.String(),
+		OperatorStateRetrieverAddr: contractAddrs.OperatorStateRetriever.String(),
+		RewardsCoordinatorAddress:  contractAddrs.RewardsCoordinator.String(),
+		AvsName:                    "exampleAvs",
+		PromMetricsIpPortAddress:   ":9090",
+	}
+
+	t.Run("register as an operator", func(t *testing.T) {
+		// Fund the new address with 5 ether
+		fundedAccount := "0x408EfD9C90d59298A9b32F4441aC9Df6A2d8C3E1"
+		fundedPrivateKeyHex := "3339854a8622364bcd5650fa92eac82d5dccf04089f5575a761c9b7d3c405b1c"
+		richPrivateKeyHex := "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+		code, _, err := anvilC.Exec(
+			context.Background(),
+			[]string{"cast",
+				"send",
+				"0x408EfD9C90d59298A9b32F4441aC9Df6A2d8C3E1",
+				"--value",
+				"5ether",
+				"--private-key",
+				richPrivateKeyHex,
+			},
+		)
+		assert.NoError(t, err)
+		assert.Equal(t, 0, code)
+
+		ecdsaPrivateKey, err := crypto.HexToECDSA(fundedPrivateKeyHex)
+		require.NoError(t, err)
+
+		clients, err := clients.BuildAll(
+			chainioConfig,
+			ecdsaPrivateKey,
+			logger,
+		)
+		require.NoError(t, err)
+
+		operator :=
+			types.Operator{
+				Address:                   fundedAccount,
+				DelegationApproverAddress: "0xd5e099c71b797516c10ed0f0d895f429c2781142",
+				StakerOptOutWindowBlocks:  100,
+				MetadataUrl:               "https://madhur-test-public.s3.us-east-2.amazonaws.com/metadata.json",
+			}
+
+		receipt, err := clients.ElChainWriter.RegisterAsOperator(context.Background(), operator, true)
+		assert.NoError(t, err)
+		assert.True(t, receipt.Status == 1)
+	})
+
+	t.Run("register as an operator already registered", func(t *testing.T) {
+		operatorAddress := "0x408EfD9C90d59298A9b32F4441aC9Df6A2d8C3E1"
+		operatorPrivateKeyHex := "3339854a8622364bcd5650fa92eac82d5dccf04089f5575a761c9b7d3c405b1c"
+
+		ecdsaPrivateKey, err := crypto.HexToECDSA(operatorPrivateKeyHex)
+		require.NoError(t, err)
+
+		clients, err := clients.BuildAll(
+			chainioConfig,
+			ecdsaPrivateKey,
+			logger,
+		)
+		require.NoError(t, err)
+
+		operator :=
+			types.Operator{
+				Address:                   operatorAddress,
+				DelegationApproverAddress: "0xd5e099c71b797516c10ed0f0d895f429c2781142",
+				StakerOptOutWindowBlocks:  100,
+				MetadataUrl:               "https://madhur-test-public.s3.us-east-2.amazonaws.com/metadata.json",
+			}
+
+		_, err = clients.ElChainWriter.RegisterAsOperator(context.Background(), operator, true)
+		assert.Error(t, err)
+	})
+}
+
 func TestChainWriter(t *testing.T) {
 	anvilStateFileName := "contracts-deployed-anvil-state.json"
 	anvilC, err := testutils.StartAnvilContainer(anvilStateFileName)
@@ -28,8 +121,8 @@ func TestChainWriter(t *testing.T) {
 	require.NoError(t, err)
 	logger := logging.NewTextSLogger(os.Stdout, &logging.SLoggerOptions{Level: slog.LevelDebug})
 
-	privateKeyHex := "3339854a8622364bcd5650fa92eac82d5dccf04089f5575a761c9b7d3c405b1c"
-	addressHex := "0x408EfD9C90d59298A9b32F4441aC9Df6A2d8C3E1"
+	privateKeyHex := "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+	addressHex := "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
 	ecdsaPrivateKey, err := crypto.HexToECDSA(privateKeyHex)
 	require.NoError(t, err)
 
@@ -46,7 +139,6 @@ func TestChainWriter(t *testing.T) {
 		PromMetricsIpPortAddress:   ":9090",
 	}
 
-	//// Setup el_chain_reader
 	clients, err := clients.BuildAll(
 		chainioConfig,
 		ecdsaPrivateKey,
@@ -70,44 +162,39 @@ func TestChainWriter(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, 0, code)
 
-	// Define an operator
-	operator :=
-		types.Operator{
-			Address:                   addressHex,
-			DelegationApproverAddress: "0xd5e099c71b797516c10ed0f0d895f429c2781142",
-			StakerOptOutWindowBlocks:  100,
-			MetadataUrl:               "https://madhur-test-public.s3.us-east-2.amazonaws.com/metadata.json",
+	t.Run("update operator details", func(t *testing.T) {
+		walletModified, err := crypto.HexToECDSA("2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6")
+		assert.NoError(t, err)
+		walletModifiedAddress := crypto.PubkeyToAddress(walletModified.PublicKey)
+
+		operatorModified := types.Operator{
+			Address:                   walletModifiedAddress.Hex(),
+			DelegationApproverAddress: walletModifiedAddress.Hex(),
+			StakerOptOutWindowBlocks:  101,
+			MetadataUrl:               "eigensdk-go",
 		}
 
-	// Test 1: Register as an operator
-	receipt, err := clients.ElChainWriter.RegisterAsOperator(context.Background(), operator, true)
-	assert.NoError(t, err)
-	assert.True(t, receipt.Status == 1)
+		receipt, err := clients.ElChainWriter.UpdateOperatorDetails(context.Background(), operatorModified, true)
+		assert.NoError(t, err)
+		assert.True(t, receipt.Status == 1)
+	})
 
-	// Test 2: Update operator details
-	walletModified, err := crypto.HexToECDSA("2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6")
-	assert.NoError(t, err)
-	walletModifiedAddress := crypto.PubkeyToAddress(walletModified.PublicKey)
+	t.Run("update metadata URI", func(t *testing.T) {
+		receipt, err := clients.ElChainWriter.UpdateMetadataURI(context.Background(), "https://0.0.0.0", true)
+		assert.NoError(t, err)
+		assert.True(t, receipt.Status == 1)
+	})
 
-	operatorModified := types.Operator{
-		Address:                   walletModifiedAddress.Hex(),
-		DelegationApproverAddress: walletModifiedAddress.Hex(),
-		StakerOptOutWindowBlocks:  101,
-		MetadataUrl:               "eigensdk-go",
-	}
-
-	receipt, err = clients.ElChainWriter.UpdateOperatorDetails(context.Background(), operatorModified, true)
-	assert.NoError(t, err)
-	assert.True(t, receipt.Status == 1)
-
-	// Test 3: Update metadata URI
-	receipt, err = clients.ElChainWriter.UpdateMetadataURI(context.Background(), "https://0.0.0.0", true)
-	assert.NoError(t, err)
-	assert.True(t, receipt.Status == 1)
+	t.Run("deposit ERC20 into strategy", func(t *testing.T) {
+		amount := big.NewInt(1)
+		receipt, err := clients.ElChainWriter.DepositERC20IntoStrategy(context.Background(), contractAddrs.Erc20MockStrategy, amount, true)
+		assert.NoError(t, err)
+		assert.True(t, receipt.Status == 1)
+	})
 
 	t.Run("set claimer for", func(t *testing.T) {
 		claimer := common.HexToAddress("0x1234567890123456789012345678901234567890")
-		receipt, err = clients.ElChainWriter.SetClaimerFor(context.Background(), claimer, true)
+		receipt, err := clients.ElChainWriter.SetClaimerFor(context.Background(), claimer, true)
 		assert.NoError(t, err)
 		assert.True(t, receipt.Status == 1)
 	})
