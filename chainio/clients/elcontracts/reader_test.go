fc1593a21a67d9e39252f3e492e63607c35c7151
tomasarrachea <tomas.arrachea@lambdaclass.com>
2024-10-02 18:08:44 -0300

    WIP elcontracts reader tests

---
+++ chainio/clients/elcontracts/reader_test.go
@@ -0,0 +1,70 @@
+package elcontracts_test
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"testing"
+
+	"github.com/Layr-Labs/eigensdk-go/chainio/clients"
+	"github.com/Layr-Labs/eigensdk-go/logging"
+	"github.com/Layr-Labs/eigensdk-go/testutils"
+	"github.com/Layr-Labs/eigensdk-go/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestChainReader(t *testing.T) {
+	anvilStateFileName := "contracts-deployed-anvil-state.json"
+	anvilC, err := testutils.StartAnvilContainer(anvilStateFileName)
+	require.NoError(t, err)
+
+	anvilHttpEndpoint, err := anvilC.Endpoint(context.Background(), "http")
+	require.NoError(t, err)
+
+	anvilWsEndpoint, err := anvilC.Endpoint(context.Background(), "ws")
+	require.NoError(t, err)
+	logger := logging.NewTextSLogger(os.Stdout, &logging.SLoggerOptions{Level: slog.LevelDebug})
+
+	privateKeyHex := "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+	ecdsaPrivateKey, err := crypto.HexToECDSA(privateKeyHex)
+	require.NoError(t, err)
+
+	contractAddrs := testutils.GetContractAddressesFromContractRegistry(anvilHttpEndpoint)
+	require.NoError(t, err)
+
+	chainioConfig := clients.BuildAllConfig{
+		EthHttpUrl:                 anvilHttpEndpoint,
+		EthWsUrl:                   anvilWsEndpoint,
+		RegistryCoordinatorAddr:    contractAddrs.RegistryCoordinator.String(),
+		OperatorStateRetrieverAddr: contractAddrs.OperatorStateRetriever.String(),
+		// RewardsCoordinatorAddress:  contractAddrs.RewardsCoordinator.String(),
+		AvsName:                  "exampleAvs",
+		PromMetricsIpPortAddress: ":9090",
+	}
+
+	clients, err := clients.BuildAll(
+		chainioConfig,
+		ecdsaPrivateKey,
+		logger,
+	)
+	require.NoError(t, err)
+
+	t.Run("is operator registered", func(t *testing.T) {
+		walletModified, err := crypto.HexToECDSA("2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6")
+		assert.NoError(t, err)
+		walletModifiedAddress := crypto.PubkeyToAddress(walletModified.PublicKey)
+
+		operatorModified := types.Operator{
+			Address:                   walletModifiedAddress.Hex(),
+			DelegationApproverAddress: walletModifiedAddress.Hex(),
+			StakerOptOutWindowBlocks:  101,
+			MetadataUrl:               "eigensdk-go",
+		}
+
+		receipt, err := clients.ElChainReader.IsOperatorRegistered(context.Background(), operatorModified, true)
+		assert.NoError(t, err)
+		assert.True(t, receipt.Status == 1)
+	})
+}
