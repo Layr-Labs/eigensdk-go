99afc9aa4d04ed584abf3f6dc5b5c5856bced851
tomasarrachea <tomas.arrachea@lambdaclass.com>
2024-10-03 16:45:33 -0300

    refactor tests client setup

--- chainio/clients/avsregistry/reader_test.go
+++ chainio/clients/avsregistry/reader_test.go
@@ -1,42 +1,64 @@
-package avsregistry
+package avsregistry_test
 
 import (
 	"context"
-	"log/slog"
 	"math/big"
 	"os"
 	"testing"
 
+	"github.com/Layr-Labs/eigensdk-go/chainio/clients"
 	"github.com/Layr-Labs/eigensdk-go/logging"
 	"github.com/Layr-Labs/eigensdk-go/testutils"
 	"github.com/Layr-Labs/eigensdk-go/types"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/stretchr/testify/require"
 )
 
-func TestReaderMethods(t *testing.T) {
-	anvilStateFileName := "contracts-deployed-anvil-state.json"
-	anvilC, err := testutils.StartAnvilContainer(anvilStateFileName)
+// Starts an anvil container and builds the ChainIO Clients for testing.
+func BuildTestClients(t *testing.T) *clients.Clients {
+	testConfig := testutils.GetDefaultTestConfig()
+	anvilC, err := testutils.StartAnvilContainer(testConfig.AnvilStateFileName)
 	require.NoError(t, err)
 
 	anvilHttpEndpoint, err := anvilC.Endpoint(context.Background(), "http")
 	require.NoError(t, err)
 
-	contractAddrs := testutils.GetContractAddressesFromContractRegistry(anvilHttpEndpoint)
-	ethHttpClient, err := ethclient.Dial(anvilHttpEndpoint)
+	anvilWsEndpoint, err := anvilC.Endpoint(context.Background(), "ws")
 	require.NoError(t, err)
+	logger := logging.NewTextSLogger(os.Stdout, &logging.SLoggerOptions{Level: testConfig.LogLevel})
+
+	privateKeyHex := "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+	ecdsaPrivateKey, err := crypto.HexToECDSA(privateKeyHex)
+	require.NoError(t, err)
+
+	contractAddrs := testutils.GetContractAddressesFromContractRegistry(anvilHttpEndpoint)
+	require.NoError(t, err)
+
+	chainioConfig := clients.BuildAllConfig{
+		EthHttpUrl:                 anvilHttpEndpoint,
+		EthWsUrl:                   anvilWsEndpoint,
+		RegistryCoordinatorAddr:    contractAddrs.RegistryCoordinator.String(),
+		OperatorStateRetrieverAddr: contractAddrs.OperatorStateRetriever.String(),
+		AvsName:                    "exampleAvs",
+		PromMetricsIpPortAddress:   ":9090",
+	}
+
+	clients, err := clients.BuildAll(
+		chainioConfig,
+		ecdsaPrivateKey,
+		logger,
+	)
+	require.NoError(t, err)
+	return clients
+}
+
+func TestReaderMethods(t *testing.T) {
+	clients := BuildTestClients(t)
+	chainReader := clients.ReadClients.AvsRegistryChainReader
 
 	quorumNumbers := types.QuorumNums{0}
-
-	logger := logging.NewTextSLogger(os.Stdout, &logging.SLoggerOptions{Level: slog.LevelDebug})
-
-	chainReader, err := NewReaderFromConfig(Config{
-		RegistryCoordinatorAddress:    contractAddrs.RegistryCoordinator,
-		OperatorStateRetrieverAddress: contractAddrs.OperatorStateRetriever,
-	}, ethHttpClient, logger)
-	require.NoError(t, err)
 
 	t.Run("get quorum state", func(t *testing.T) {
 		count, err := chainReader.GetQuorumCount(&bind.CallOpts{})
--- chainio/clients/avsregistry/writer_test.go
+++ chainio/clients/avsregistry/writer_test.go
@@ -1,63 +1,27 @@
-package avsregistry
+package avsregistry_test
 
 import (
 	"context"
-	"log/slog"
-	"os"
 	"testing"
-	"time"
 
-	"github.com/Layr-Labs/eigensdk-go/chainio/clients/wallet"
-	"github.com/Layr-Labs/eigensdk-go/chainio/txmgr"
 	chainioutils "github.com/Layr-Labs/eigensdk-go/chainio/utils"
 	"github.com/Layr-Labs/eigensdk-go/crypto/bls"
-	"github.com/Layr-Labs/eigensdk-go/logging"
-	"github.com/Layr-Labs/eigensdk-go/signerv2"
-	"github.com/Layr-Labs/eigensdk-go/testutils"
 	"github.com/Layr-Labs/eigensdk-go/types"
 	gethcommon "github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/stretchr/testify/require"
 )
 
 func TestWriterMethods(t *testing.T) {
-	anvilStateFileName := "contracts-deployed-anvil-state.json"
-	anvilC, err := testutils.StartAnvilContainer(anvilStateFileName)
-	require.NoError(t, err)
-
-	anvilHttpEndpoint, err := anvilC.Endpoint(context.Background(), "http")
-	require.NoError(t, err)
-
-	contractAddrs := testutils.GetContractAddressesFromContractRegistry(anvilHttpEndpoint)
-	ethHttpClient, err := ethclient.Dial(anvilHttpEndpoint)
-	require.NoError(t, err)
-
-	logger := logging.NewTextSLogger(os.Stdout, &logging.SLoggerOptions{Level: slog.LevelDebug})
-
-	ecdsaPrivKeyHex := "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
-	ecdsaPrivateKey, err := crypto.HexToECDSA(ecdsaPrivKeyHex)
-	require.NoError(t, err)
-
-	rpcCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	chainid, err := ethHttpClient.ChainID(rpcCtx)
-	require.NoError(t, err)
-
-	signerV2, addr, err := signerv2.SignerFromConfig(signerv2.Config{PrivateKey: ecdsaPrivateKey}, chainid)
-	require.NoError(t, err)
-
-	pkWallet, err := wallet.NewPrivateKeyWallet(ethHttpClient, signerV2, addr, logger)
-	require.NoError(t, err)
-	txMgr := txmgr.NewSimpleTxManager(pkWallet, ethHttpClient, logger, addr)
-
-	chainWriter, err := NewWriterFromConfig(Config{
-		RegistryCoordinatorAddress:    contractAddrs.RegistryCoordinator,
-		OperatorStateRetrieverAddress: contractAddrs.OperatorStateRetriever,
-	}, ethHttpClient, txMgr, logger)
-	require.NoError(t, err)
+	clients := BuildTestClients(t)
+	chainWriter := clients.AvsRegistryChainWriter
 
 	keypair, err := bls.NewKeyPairFromString("0x01")
+	require.NoError(t, err)
+
+	addr := gethcommon.HexToAddress("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266")
+	ecdsaPrivKeyHex := "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+	ecdsaPrivateKey, err := crypto.HexToECDSA(ecdsaPrivKeyHex)
 	require.NoError(t, err)
 
 	quorumNumbers := types.QuorumNums{0}
