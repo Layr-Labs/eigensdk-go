f8d9a93fe84ab0aeaa883fe71360eec75a849061
tomasarrachea <tomas.arrachea@lambdaclass.com>
2024-10-04 14:51:00 -0300

    use BuildTestClients util on clients tests

--- chainio/clients/elcontracts/writer_test.go
+++ chainio/clients/elcontracts/writer_test.go
@@ -2,7 +2,6 @@ package elcontracts_test
 
 import (
 	"context"
-	"log/slog"
 	"math/big"
 	"os"
 	"testing"
@@ -10,6 +9,7 @@ import (
 	"github.com/Layr-Labs/eigensdk-go/chainio/clients"
 	"github.com/Layr-Labs/eigensdk-go/logging"
 	"github.com/Layr-Labs/eigensdk-go/testutils"
+	"github.com/Layr-Labs/eigensdk-go/testutils/testclients"
 	"github.com/Layr-Labs/eigensdk-go/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/stretchr/testify/assert"
@@ -17,14 +17,14 @@ import (
 )
 
 func TestRegisterOperator(t *testing.T) {
-	anvilStateFileName := "contracts-deployed-anvil-state.json"
-	anvilC, err := testutils.StartAnvilContainer(anvilStateFileName)
+	testConfig := testutils.GetDefaultTestConfig()
+	anvilC, err := testutils.StartAnvilContainer(testConfig.AnvilStateFileName)
 	require.NoError(t, err)
 	anvilHttpEndpoint, err := anvilC.Endpoint(context.Background(), "http")
 	require.NoError(t, err)
 	anvilWsEndpoint, err := anvilC.Endpoint(context.Background(), "ws")
 	require.NoError(t, err)
-	logger := logging.NewTextSLogger(os.Stdout, &logging.SLoggerOptions{Level: slog.LevelDebug})
+	logger := logging.NewTextSLogger(os.Stdout, &logging.SLoggerOptions{Level: testConfig.LogLevel})
 
 	contractAddrs := testutils.GetContractAddressesFromContractRegistry(anvilHttpEndpoint)
 	require.NoError(t, err)
@@ -108,56 +108,8 @@ func TestRegisterOperator(t *testing.T) {
 }
 
 func TestChainWriter(t *testing.T) {
-	anvilStateFileName := "contracts-deployed-anvil-state.json"
-	anvilC, err := testutils.StartAnvilContainer(anvilStateFileName)
-	require.NoError(t, err)
-
-	anvilHttpEndpoint, err := anvilC.Endpoint(context.Background(), "http")
-	require.NoError(t, err)
-
-	anvilWsEndpoint, err := anvilC.Endpoint(context.Background(), "ws")
-	require.NoError(t, err)
-	logger := logging.NewTextSLogger(os.Stdout, &logging.SLoggerOptions{Level: slog.LevelDebug})
-
-	privateKeyHex := "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
-	addressHex := "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
-	ecdsaPrivateKey, err := crypto.HexToECDSA(privateKeyHex)
-	require.NoError(t, err)
-
+	clients, anvilHttpEndpoint := testclients.BuildTestClients(t)
 	contractAddrs := testutils.GetContractAddressesFromContractRegistry(anvilHttpEndpoint)
-	require.NoError(t, err)
-
-	chainioConfig := clients.BuildAllConfig{
-		EthHttpUrl:                 anvilHttpEndpoint,
-		EthWsUrl:                   anvilWsEndpoint,
-		RegistryCoordinatorAddr:    contractAddrs.RegistryCoordinator.String(),
-		OperatorStateRetrieverAddr: contractAddrs.OperatorStateRetriever.String(),
-		AvsName:                    "exampleAvs",
-		PromMetricsIpPortAddress:   ":9090",
-	}
-
-	clients, err := clients.BuildAll(
-		chainioConfig,
-		ecdsaPrivateKey,
-		logger,
-	)
-	require.NoError(t, err)
-
-	// Fund the address with 5 ether
-	richPrivateKeyHex := "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
-	code, _, err := anvilC.Exec(
-		context.Background(),
-		[]string{"cast",
-			"send",
-			addressHex,
-			"--value",
-			"5ether",
-			"--private-key",
-			richPrivateKeyHex,
-		},
-	)
-	assert.NoError(t, err)
-	assert.Equal(t, 0, code)
 
 	t.Run("update operator details", func(t *testing.T) {
 		walletModified, err := crypto.HexToECDSA("2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6")
