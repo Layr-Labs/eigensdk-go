521033d28904f86e5e8f9acb1709ef1d07f27d55
Pablo Deymonnaz <deymonnaz@gmail.com>
2024-10-05 21:11:31 -0300

    Reduce permissions for file created according to CWE-276 (#355)

--- crypto/bls/attestation.go
+++ crypto/bls/attestation.go
@@ -192,11 +192,11 @@ func (k *KeyPair) SaveToFile(path string, password string) error {
 	}
 
 	dir := filepath.Dir(path)
-	if err := os.MkdirAll(dir, 0755); err != nil {
+	if err := os.MkdirAll(dir, 0750); err != nil {
 		fmt.Println("Error creating directories:", err)
 		return err
 	}
-	err = os.WriteFile(path, data, 0644)
+	err = os.WriteFile(path, data, 0600)
 	if err != nil {
 		return err
 	}
