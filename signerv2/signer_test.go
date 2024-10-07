a923f5a10d76f21ca6387d2405fd8aa7ef440f74
tomasarrachea <tomas.arrachea@lambdaclass.com>
2024-10-07 12:16:44 -0300

    private key signer test

--- signerv2/signer_test.go
+++ signerv2/signer_test.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 
 	"github.com/Layr-Labs/eigensdk-go/signerv2"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/stretchr/testify/require"
 )
@@ -15,6 +17,21 @@ func TestPrivateKeySignerFn(t *testing.T) {
 	require.NoError(t, err)
 	chainID := big.NewInt(1)
 
-	_, err = signerv2.PrivateKeySignerFn(privateKey, chainID)
+	signer, err := signerv2.PrivateKeySignerFn(privateKey, chainID)
 	require.NoError(t, err)
+
+	address := crypto.PubkeyToAddress(privateKey.PublicKey)
+	tx := types.NewTx(&types.DynamicFeeTx{
+		Nonce:   0,
+		Value:   big.NewInt(0),
+		ChainID: chainID,
+		Data:    common.Hex2Bytes("6057361d00000000000000000000000000000000000000000000000000000000000f4240"),
+	})
+	signedTx, err := signer(address, tx)
+	require.NoError(t, err)
+
+	// Verify the sender address of the signed transaction
+	from, err := types.Sender(types.LatestSignerForChainID(chainID), signedTx)
+	require.NoError(t, err)
+	require.Equal(t, address, from)
 }
