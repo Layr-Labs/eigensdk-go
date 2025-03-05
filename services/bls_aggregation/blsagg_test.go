b2e665e3c4b44f1ce8e1e21941210c061bdcd82f
maximopalopoli <mpalopoli@fi.uba.ar>
2025-03-05 16:40:12 -0300

    Reduce context timeout to reach deadlineExceededError

--- services/bls_aggregation/blsagg_test.go
+++ services/bls_aggregation/blsagg_test.go
@@ -961,8 +961,7 @@ func TestBlsAgg(t *testing.T) {
 
 	// this is an edge case as typically we would send new tasks and listen for task responses in a for select loop
 	// but this test makes sure the context deadline exceeded can get us out of a deadlock
-	t.Run(
-		"send new signedTaskDigest before listen on responseChan - context timeout cancels the request to prevent deadlock",
+	t.Run("send new signedTaskDigest before listen on responseChan - context timeout cancels the request to prevent deadlock",
 		func(t *testing.T) {
 			testOperator1 := types.TestOperator{
 				OperatorId:     types.OperatorId{1},
@@ -1009,7 +1008,7 @@ func TestBlsAgg(t *testing.T) {
 			taskResponseDigest2, err := hashFunction(taskResponse2)
 			require.Nil(t, err)
 			blsSigOp2 := testOperator2BlsKeypair.SignMessage(taskResponseDigest2)
-			ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+			ctx, cancel := context.WithTimeout(context.Background(), 50*time.Millisecond)
 			defer cancel()
 			taskSignature2 := NewTaskSignature(taskIndex, taskResponse2, blsSigOp2, testOperator2OperatorId)
 			err = blsAggServ.ProcessNewSignature(
