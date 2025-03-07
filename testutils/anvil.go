8ede0205f1d55bd2646bdcd2dd6095340d3a8515
maximopalopoli <mpalopoli@fi.uba.ar>
2025-03-07 11:27:18 -0300

    Add testcase registering an operator to eigenlayer with m2 flow

--- testutils/anvil.go
+++ testutils/anvil.go
@@ -68,6 +68,45 @@ func StartAnvilContainer(anvilStateFileName string) (testcontainers.Container, e
 	return anvilC, nil
 }
 
+func StartM2AnvilContainer(anvilStateFileName string) (testcontainers.Container, error) {
+	ctx := context.Background()
+	req := testcontainers.ContainerRequest{
+		Image:        "ghcr.io/foundry-rs/foundry:stable@sha256:daeeaaf4383ee0cbfc9f31f079a04ffb0123e49e5f67f2a20b5ce1ac1959a4d6",
+		Entrypoint:   []string{"anvil"},
+		Cmd:          []string{"--host", "0.0.0.0", "--base-fee", "0", "--gas-price", "0"},
+		ExposedPorts: []string{"8545/tcp"},
+		WaitingFor:   wait.ForLog("Listening on"),
+	}
+	if anvilStateFileName != "" {
+		fmt.Println("Starting Anvil container with state file: ", anvilStateFileName)
+		req.Cmd = append(req.Cmd, "--load-state", "/root/.anvil/state.json")
+		_, curFilePath, _, _ := runtime.Caller(0)
+		req.Files = []testcontainers.ContainerFile{
+			{
+				HostFilePath:      filepath.Join(curFilePath, "../../M2-contracts/anvil/", anvilStateFileName),
+				ContainerFilePath: "/root/.anvil/state.json",
+				FileMode:          0644, // Adjust the FileMode according to your requirements
+			},
+		}
+	}
+	anvilC, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// Still need to advance the chain by at least 1 block b/c some tests need to query the latest block,
+	// and the blocks dumped/loaded by anvil don't contain full transactions, which leads to panics in tests.
+	// See https://github.com/foundry-rs/foundry/issues/8213, which will hopefully get fixed soon.
+	if anvilStateFileName != "" {
+		AdvanceChainByNBlocksExecInContainer(ctx, 1, anvilC)
+	}
+
+	return anvilC, nil
+}
+
 type ContractAddresses struct {
 	ServiceManager         common.Address
 	RegistryCoordinator    common.Address
@@ -154,6 +193,78 @@ func GetContractAddressesFromContractRegistry(ethHttpUrl string) (mockAvsContrac
 		Erc20MockStrategy:      erc20MockStrategyAddr,
 		RewardsCoordinator:     rewardsCoordinatorAddr,
 		PermissionController:   permissionControllerAddr,
+	}
+	return mockAvsContracts
+}
+
+func GetM2ContractAddressesFromContractRegistry(ethHttpUrl string) (mockAvsContracts ContractAddresses) {
+	ethHttpClient, err := ethclient.Dial(ethHttpUrl)
+	if err != nil {
+		panic(err)
+	}
+	// The ContractsRegistry contract should always be deployed at this address on anvil
+	// it's the address of the contract created at nonce 0 by the first anvil account
+	// (0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266)
+	contractsRegistry, err := contractreg.NewContractContractsRegistry(
+		common.HexToAddress("0x5FbDB2315678afecb367f032d93F642f64180aa3"),
+		ethHttpClient,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	mockAvsServiceManagerAddr, err := contractsRegistry.Contracts(&bind.CallOpts{}, "mockAvsServiceManager")
+	if err != nil {
+		panic(err)
+	}
+	if mockAvsServiceManagerAddr == (common.Address{}) {
+		panic("mockAvsServiceManagerAddr is empty")
+	}
+	mockAvsRegistryCoordinatorAddr, err := contractsRegistry.Contracts(&bind.CallOpts{}, "mockAvsRegistryCoordinator")
+	if err != nil {
+		panic(err)
+	}
+	if mockAvsRegistryCoordinatorAddr == (common.Address{}) {
+		panic("mockAvsRegistryCoordinatorAddr is empty")
+	}
+	mockAvsOperatorStateRetrieverAddr, err := contractsRegistry.Contracts(
+		&bind.CallOpts{},
+		"mockAvsOperatorStateRetriever",
+	)
+	if err != nil {
+		panic(err)
+	}
+	if mockAvsOperatorStateRetrieverAddr == (common.Address{}) {
+		panic("mockAvsOperatorStateRetrieverAddr is empty")
+	}
+	delegationManagerAddr, err := contractsRegistry.Contracts(&bind.CallOpts{}, "delegationManager")
+	if err != nil {
+		panic(err)
+	}
+	if delegationManagerAddr == (common.Address{}) {
+		panic("delegationManagerAddr is empty")
+	}
+	erc20MockStrategyAddr, err := contractsRegistry.Contracts(&bind.CallOpts{}, "erc20MockStrategy")
+	if err != nil {
+		panic(err)
+	}
+	if erc20MockStrategyAddr == (common.Address{}) {
+		panic("erc20MockStrategyAddr is empty")
+	}
+	rewardsCoordinatorAddr, err := contractsRegistry.Contracts(&bind.CallOpts{}, "rewardsCoordinator")
+	if err != nil {
+		panic(err)
+	}
+	if rewardsCoordinatorAddr == (common.Address{}) {
+		panic("rewardsCoordinatorAddr is empty")
+	}
+	mockAvsContracts = ContractAddresses{
+		ServiceManager:         mockAvsServiceManagerAddr,
+		RegistryCoordinator:    mockAvsRegistryCoordinatorAddr,
+		OperatorStateRetriever: mockAvsOperatorStateRetrieverAddr,
+		DelegationManager:      delegationManagerAddr,
+		Erc20MockStrategy:      erc20MockStrategyAddr,
+		RewardsCoordinator:     rewardsCoordinatorAddr,
 	}
 	return mockAvsContracts
 }
