e3611c96a3f4b0bfc57bcbe4fd16ecd8d087a762
maximopalopoli <mpalopoli@fi.uba.ar>
2025-03-18 18:08:51 -0300

    forge fmt on contracts

--- contracts/src/LegacyRegistryCoordinator.sol
+++ contracts/src/LegacyRegistryCoordinator.sol
@@ -3,7 +3,8 @@ pragma solidity ^0.8.27;
 
 import {RegistryCoordinator} from "eigenlayer-middleware/src/RegistryCoordinator.sol";
 import {
-    IRegistryCoordinator, IRegistryCoordinatorTypes
+    IRegistryCoordinator,
+    IRegistryCoordinatorTypes
 } from "eigenlayer-middleware/src/interfaces/IRegistryCoordinator.sol";
 import {SlashingRegistryCoordinator} from "eigenlayer-middleware/src/SlashingRegistryCoordinator.sol";
 
@@ -27,19 +28,19 @@ contract LegacyRegistryCoordinator is RegistryCoordinator {
         IAllocationManager _allocationManager,
         IPauserRegistry _pauserRegistry
     )
-    RegistryCoordinator(
-        IRegistryCoordinatorTypes.RegistryCoordinatorParams({
-            serviceManager: _serviceManager,
-            slashingParams: IRegistryCoordinatorTypes.SlashingRegistryParams({
-                stakeRegistry: _stakeRegistry,
-                blsApkRegistry: _blsApkRegistry,
-                indexRegistry: _indexRegistry,
-                socketRegistry: _socketRegistry,
-                allocationManager: _allocationManager,
-                pauserRegistry: _pauserRegistry
+        RegistryCoordinator(
+            IRegistryCoordinatorTypes.RegistryCoordinatorParams({
+                serviceManager: _serviceManager,
+                slashingParams: IRegistryCoordinatorTypes.SlashingRegistryParams({
+                    stakeRegistry: _stakeRegistry,
+                    blsApkRegistry: _blsApkRegistry,
+                    indexRegistry: _indexRegistry,
+                    socketRegistry: _socketRegistry,
+                    allocationManager: _allocationManager,
+                    pauserRegistry: _pauserRegistry
+                })
             })
-        })
-    )
+        )
     {}
 
     /// Disables operator sets mode
