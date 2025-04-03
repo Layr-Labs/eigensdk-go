334cf58486588397bc88926dc11f0722e29509c2
maximopalopoli <mpalopoli@fi.uba.ar>
2025-04-03 17:22:27 -0300

    Add Initial aggregator generic implementation

---
+++ aggregator/aggregator.go
@@ -0,0 +1,278 @@
+package aggregator
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"math/big"
+	"net/http"
+	"net/rpc"
+
+	"github.com/Layr-Labs/eigensdk-go/crypto/bls"
+	"github.com/Layr-Labs/eigensdk-go/logging"
+	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"golang.org/x/crypto/sha3"
+
+	sdkclients "github.com/Layr-Labs/eigensdk-go/chainio/clients"
+	"github.com/Layr-Labs/eigensdk-go/chainio/clients/avsregistry"
+	regcoord "github.com/Layr-Labs/eigensdk-go/contracts/bindings/RegistryCoordinator"
+	avsregistryservice "github.com/Layr-Labs/eigensdk-go/services/avsregistry"
+	blsagg "github.com/Layr-Labs/eigensdk-go/services/bls_aggregation"
+	oprsinfoserv "github.com/Layr-Labs/eigensdk-go/services/operatorsinfo"
+	sdktypes "github.com/Layr-Labs/eigensdk-go/types"
+)
+
+const (
+	// number of blocks after which a task is considered expired
+	// this hardcoded here because it's also hardcoded in the contracts, but should
+	// ideally be fetched from the contracts
+	//taskChallengeWindowBlock = 100
+	//blockTimeSeconds         = 12 * time.Second
+	avsName                  = "incredible-squaring"
+)
+
+type Aggregator struct {
+	logger           logging.Logger
+	serverIpPortAddr string
+	avsWriter        *avsregistry.ChainWriter
+	// aggregation related fields
+	blsAggregationService blsagg.BlsAggregationService
+	//tasks                 map[types.TaskIndex]cstaskmanager.IIncredibleSquaringTaskManagerTask
+	//tasksMu               sync.RWMutex
+	//avsSubscriber         chainio.AvsSubscriberer
+	//newTaskCreatedChan    chan *cstaskmanager.ContractIncredibleSquaringTaskManagerNewTaskCreated
+    wsRpcUrl string
+	taskProcessor TaskProcessor
+	eventHash common.Hash
+}
+
+// NewAggregator creates a new Aggregator with the provided config.
+func NewAggregator(c AggregatorConfig, taskProcessor TaskProcessor) (*Aggregator, error) {
+	avsConfig := avsregistry.Config{
+		RegistryCoordinatorAddress: c.RegistryCoordinatorAddress,
+		OperatorStateRetrieverAddress: c.OperatorStateRetrieverAddress,
+		ServiceManagerAddress: c.ServiceManagerAddress,
+	}
+	avsReader, err := avsregistry.NewReaderFromConfig(avsConfig, c.EthHttpClient, c.Logger)
+	if err != nil {
+		c.Logger.Error("Cannot create avsReader", "err", err)
+		return nil, err
+	}
+
+	avsWriter, err := avsregistry.NewWriterFromConfig(avsConfig, c.EthHttpClient, c.TxMgr, c.Logger)
+	if err != nil {
+		c.Logger.Errorf("Cannot create avsWriter", "err", err)
+		return nil, err
+	}
+
+	chainioConfig := sdkclients.BuildAllConfig{
+		EthHttpUrl:                 c.httpRpcUrl,
+		EthWsUrl:                   c.wsRpcUrl,
+		RegistryCoordinatorAddr:    c.RegistryCoordinatorAddress.String(),
+		OperatorStateRetrieverAddr: c.OperatorStateRetrieverAddress.String(),
+		AvsName:                    avsName,
+		PromMetricsIpPortAddress:   ":9090",
+
+		DontUseAllocationManager: true,
+	}
+
+	clients, err := sdkclients.BuildAll(chainioConfig, c.EcdsaPrivateKey, c.Logger)
+	if err != nil {
+		c.Logger.Errorf("Cannot create sdk clients", "err", err)
+		return nil, err
+	}
+
+	operatorPubkeysService := oprsinfoserv.NewOperatorsInfoServiceInMemory(
+		context.Background(),
+		clients.AvsRegistryChainSubscriber,
+		clients.AvsRegistryChainReader,
+		nil,
+		oprsinfoserv.Opts{},
+		c.Logger,
+	)
+
+	// This is the same hash function used by the operator to hash the task response before signing it.
+	hashFunction := func(taskResponse sdktypes.TaskResponse) (sdktypes.TaskResponseDigest, error) {
+		// The order here has to match the field ordering of cstaskmanager.IIncredibleSquaringTaskManagerTaskResponse
+		taskResponseType, err := abi.NewType("tuple", "", []abi.ArgumentMarshaling{
+			{
+				Name: "referenceTaskIndex",
+				Type: "uint32",
+			},
+			{
+				Name: "numberSquared",
+				Type: "uint256",
+			},
+		})
+		if err != nil {
+			c.Logger.Error("Error creating taskResponseType")
+			return sdktypes.TaskResponseDigest{}, err
+		}
+		arguments := abi.Arguments{
+			{
+				Type: taskResponseType,
+			},
+		}
+
+		encodeTaskResponseByte, err := arguments.Pack(taskResponse)
+		if err != nil {
+			c.Logger.Error("Error Packing taskResponse", err)
+			return sdktypes.TaskResponseDigest{}, err
+		}
+
+		var taskResponseDigest [32]byte
+		hasher := sha3.NewLegacyKeccak256()
+		hasher.Write(encodeTaskResponseByte)
+		copy(taskResponseDigest[:], hasher.Sum(nil)[:32])
+
+		return taskResponseDigest, nil
+	}
+
+	avsRegistryService := avsregistryservice.NewAvsRegistryServiceChainCaller(avsReader, operatorPubkeysService, c.Logger)
+	blsAggregationService := blsagg.NewBlsAggregatorService(avsRegistryService, hashFunction, c.Logger)
+
+	return &Aggregator{
+		logger:                c.Logger,
+		serverIpPortAddr:      c.serverAddress,
+		avsWriter:             avsWriter,
+		blsAggregationService: blsAggregationService,
+		taskProcessor: taskProcessor,
+
+	}, nil
+}
+
+func (agg *Aggregator) Start(ctx context.Context) error {
+	agg.logger.Info("Starting aggregator.")
+	agg.logger.Info("Starting aggregator rpc server.")
+	go agg.startServer(ctx)
+
+//	sub := agg.avsSubscriber.SubscribeToNewTasks(agg.newTaskCreatedChan)
+
+	go agg.processTasksInit()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return nil
+//		case err := <-sub.Err():
+//			agg.logger.Error("Error in websocket subscription", "err", err)
+//			sub.Unsubscribe()
+//			sub = agg.avsSubscriber.SubscribeToNewTasks(agg.newTaskCreatedChan)
+		case blsAggServiceResp := <-agg.blsAggregationService.GetResponseChannel():
+			agg.logger.Info("Received response from blsAggregationService", "blsAggServiceResp", blsAggServiceResp)
+			agg.taskProcessor.ProcessAggregatedResponse(context.Background(), blsAggServiceResp)
+//		case newTaskCreatedLog := <-agg.newTaskCreatedChan:
+//			err := agg.processTaskGeneration(newTaskCreatedLog)
+//			if err != nil {
+//				continue
+//			}
+		}
+	}
+}
+
+// TODO: Move this to NewAggregator and Start functions
+func (agg *Aggregator) processTasksInit() error {
+	// Hear for NewTaskCreatedEvents and create New Task Metadata from that Event
+	client, err := ethclient.Dial(agg.wsRpcUrl)
+	if err != nil {
+		return errors.New("error connecting to web socket")
+	}
+
+	query := ethereum.FilterQuery{
+		Addresses: []common.Address{},
+		Topics: [][]common.Hash{{agg.eventHash}},
+	}
+
+	newTaskCreatedLogs := make(chan types.Log)
+	sub, err := client.SubscribeFilterLogs(context.Background(), query, newTaskCreatedLogs)
+	if err != nil {
+		return errors.New("error subscribing to newTaskCreated events")
+	}
+	defer sub.Unsubscribe()
+
+	for {
+		select {
+		case err := <-sub.Err():
+			return fmt.Errorf("error subscribing: %v", err)
+		case log := <-newTaskCreatedLogs:
+			go func(log types.Log) {
+				metadata, err := agg.taskProcessor.ProcessNewTask(context.Background(), log)
+				if err != nil {
+					agg.logger.Infof("Error procesando la tarea: %v", err)
+					return
+				}
+				if err := agg.blsAggregationService.InitializeNewTask(metadata); err != nil {
+					agg.logger.Infof("Error inicializando tarea: %v", err)
+				}
+			}(log)
+		}
+	}
+}
+
+func (agg *Aggregator) startServer(ctx context.Context) error {
+
+	err := rpc.Register(agg)
+	if err != nil {
+		agg.logger.Fatal("Format of service TaskManager isn't correct. ", "err", err)
+	}
+	rpc.HandleHTTP()
+	err = http.ListenAndServe(agg.serverIpPortAddr, nil)
+	if err != nil {
+		agg.logger.Fatal("ListenAndServe", "err", err)
+	}
+
+	return nil
+}
+
+type NonSignerStakesAndSignature struct {
+	NonSignerQuorumBitmapIndices []uint32
+	NonSignerPubkeys             []regcoord.BN254G1Point
+	QuorumApks                   []regcoord.BN254G1Point
+	ApkG2                        regcoord.BN254G2Point
+	Sigma                        regcoord.BN254G1Point
+	QuorumApkIndices             []uint32
+	TotalStakeIndices            []uint32
+	NonSignerStakeIndices        [][]uint32
+}
+
+type SignedTaskResponse struct {
+	TaskResponse TaskResponse
+	BlsSignature bls.Signature
+	OperatorId   sdktypes.OperatorId
+}
+
+// rpc endpoint which is called by operator
+// reply doesn't need to be checked. If there are no errors, the task response is accepted
+// rpc framework forces a reply type to exist, so we put bool as a placeholder
+func (agg *Aggregator) ProcessSignedTaskResponse(signedTaskResponse *SignedTaskResponse, reply *bool) error {
+	agg.logger.Infof("Received signed task response: %#v", signedTaskResponse)
+	taskIndex := signedTaskResponse.TaskResponse.ReferenceTaskIndex
+
+	taskSignature := blsagg.NewTaskSignature(
+		taskIndex,
+		signedTaskResponse.TaskResponse,
+		&signedTaskResponse.BlsSignature,
+		signedTaskResponse.OperatorId,
+	)
+
+	err := agg.blsAggregationService.ProcessNewSignature(context.Background(), taskSignature)
+
+	return err
+}
+
+type TaskResponse struct {
+	ReferenceTaskIndex uint32
+	NumberSquared      *big.Int
+}
+
+type B256 struct{}
+
+type TaskProcessor interface {
+	ProcessNewTask(ctx context.Context, event any) (blsagg.TaskMetadata, error)
+	ProcessTaskResponse(ctx context.Context, event any) (B256, error)
+	ProcessAggregatedResponse(ctx context.Context, response blsagg.BlsAggregationServiceResponse) error
+}
--- go.mod
+++ go.mod
@@ -1,8 +1,8 @@
 module github.com/Layr-Labs/eigensdk-go
 
-go 1.22.0
+go 1.23
 
-toolchain go1.22.5
+toolchain go1.23.5
 
 require (
 	github.com/aws/aws-sdk-go-v2/credentials v1.17.11
@@ -20,6 +20,7 @@ require (
 )
 
 require (
+	github.com/BurntSushi/toml v1.4.1-0.20240526193622-a339e1f7089c // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.1 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.5 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.5 // indirect
@@ -45,6 +46,7 @@ require (
 	go.opentelemetry.io/otel v1.24.0 // indirect
 	go.opentelemetry.io/otel/metric v1.24.0 // indirect
 	go.opentelemetry.io/otel/trace v1.24.0 // indirect
+	golang.org/x/tools v0.30.0 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20240318140521-94a12d6c2237 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
@@ -108,11 +110,12 @@ require (
 	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
 	github.com/yusufpapurcu/wmi v1.2.3 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	golang.org/x/crypto v0.32.0 // indirect
+	golang.org/x/crypto v0.32.0
 	golang.org/x/exp v0.0.0-20231110203233-9a3e6036ecaa // indirect
-	golang.org/x/sync v0.10.0
-	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/sync v0.11.0
+	golang.org/x/sys v0.30.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240318140521-94a12d6c2237 // indirect
 	google.golang.org/protobuf v1.34.2 // indirect
+	honnef.co/go/tools v0.6.1
 	rsc.io/tmplfunc v0.0.3 // indirect
 )
