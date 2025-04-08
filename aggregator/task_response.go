a0d0e5f1d6fcc046820c274a59d791c53497a856
maximopalopoli <mpalopoli@fi.uba.ar>
2025-04-08 15:25:01 -0300

    make fmt changes

--- aggregator/task_response.go
+++ aggregator/task_response.go
@@ -1,8 +1,8 @@
 package aggregator
 
 import sdktypes "github.com/Layr-Labs/eigensdk-go/types"
-	 
+
 type TaskResponse interface {
-	TaskIndex()(sdktypes.TaskIndex)
-	Digest()([256]byte)
+	TaskIndex() sdktypes.TaskIndex
+	Digest() [256]byte
 }
