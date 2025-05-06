e2cef6e00128b984a9922a164d1c1d9f0f0c67a9
maximopalopoli <mpalopoli@fi.uba.ar>
2025-05-06 15:24:26 -0300

    Add operator registration on startup

--- examples/incredible-dot-product/operator/main.go
+++ examples/incredible-dot-product/operator/main.go
@@ -21,7 +21,7 @@ type DotProductOutput struct {
 }
 
 func main() {
-	logger, err := logging.NewZapLogger(logging.Development)
+	logger, err := logging.NewZapLogger(logging.Production)
 	if err != nil {
 		panic(err)
 	}
@@ -31,18 +31,22 @@ func main() {
 		logger.Errorf("Failed to get task manager abi: %w", err)
 	}
 
+	ethHttpUrl := "http://localhost:8545"
+
+	operatorAddr := "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+
 	operatorConfig := operator.OperatorConfig{
 		Logger:         logger,
 		TaskManagerAbi: taskManagerAbi,
 
-		OperatorAddress: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+		OperatorAddress: operatorAddr,
 
 		AVSRegistryCoordinatorAddress: "0x7bc06c482dead17c0e297afbc32f6e63d3846650",
 		OperatorStateRetrieverAddress: "0x4c5859f0f772848b2d91f1d83e2fe57935348029",
 		ServiceManagerAddress:         "0x5f3f1dbd7b74c6b46e8c44f98792a1daf8d69154",
 
 		EthWsUrl:                      "ws://localhost:8545",
-		EthRpcUrl:                     "http://localhost:8545",
+		EthRpcUrl:                     ethHttpUrl,
 		AggregatorServerIpPortAddress: "localhost:8090",
 
 		BlsPrivateKeyStorePath: "keys/test.bls.key.json",
@@ -64,14 +68,19 @@ func main() {
 		return taskResponse, nil
 	}
 
+	err = RegisterOperatorOnStartup(logger)
+	if err != nil {
+		logger.Fatalf("Failed to register operator on startup: %v", err.Error())
+	}
+
 	// Setting the TaskResponseHashFn parameter in nil because I'm using the abi default encoding function
 	operator, err := operator.NewOperatorFromConfig(operatorConfig, responseCalcFunction, nil)
 	if err != nil {
-		logger.Errorf("Failed to create operator: %w", err)
+		logger.Fatalf("Failed to create operator: %w", err)
 	}
 
 	err = operator.Start(context.Background())
 	if err != nil {
-		logger.Errorf("Failure while running operator: %w", err)
+		logger.Fatalf("Failure while running operator: %w", err)
 	}
 }
