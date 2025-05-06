032e59d3fc6cc904b78b67ffdc8e3bd3db57a58c
maximopalopoli <mpalopoli@fi.uba.ar>
2025-05-06 15:51:39 -0300

    Add challenge raising to contract in challenge verifier

--- examples/incredible-dot-product/challenger/challenger_verifier.go
+++ examples/incredible-dot-product/challenger/challenger_verifier.go
@@ -1,12 +1,18 @@
 package challenger
 
 import (
+	"context"
+	"errors"
 	"math/big"
 
+	"github.com/Layr-Labs/eigensdk-go/chainio/txmgr"
 	"github.com/Layr-Labs/eigensdk-go/challenger"
 	"github.com/Layr-Labs/eigensdk-go/logging"
 	sdktypes "github.com/Layr-Labs/eigensdk-go/types"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+
+	taskmanager "github.com/Layr-Labs/eigensdk-go/examples/incredible-dot-product/contracts/bindings/IncredibleDotProductTaskManager"
 )
 
 type DotProductInput struct {
@@ -19,13 +25,27 @@ type DotProductOutput struct {
 }
 
 type ChallengeVerifier struct {
-	logger logging.Logger
+	logger              logging.Logger
+	taskManagerContract *taskmanager.ContractIncredibleDotProductTaskManager
+	txMgr               txmgr.TxManager
 }
 
-func NewChallengeVerifier(logger logging.Logger, taskManagerAddr common.Address) (ChallengeVerifier, error) {
+func NewChallengeVerifier(
+	logger logging.Logger,
+	taskManagerAddr common.Address,
+	ethclient ethclient.Client,
+	txMgr txmgr.TxManager,
+) (ChallengeVerifier, error) {
 	// Get the task manager contract with the given address
+	taskManagerContract, err := taskmanager.NewContractIncredibleDotProductTaskManager(taskManagerAddr, &ethclient)
+	if err != nil {
+		logger.Errorf("Failed to get Task Manager Contract: %w", err)
+		return ChallengeVerifier{}, err
+	}
 	return ChallengeVerifier{
-		logger: logger,
+		logger:              logger,
+		taskManagerContract: taskManagerContract,
+		txMgr:               txMgr,
 	}, nil
 }
 
@@ -46,6 +66,52 @@ func (cv ChallengeVerifier) VerifyChallenge(taskIndex uint32, task sdktypes.Gene
 	if shouldRaiseChallenge {
 		cv.logger.Infof("Response was not correct, expected %v and got %v", totalSum, receivedResponse)
 		// Call the Raise Challenge method of the on-chain contract
+
+		noSendTxOpts, err := cv.txMgr.GetNoSendTxOpts()
+		if err != nil {
+			cv.logger.Errorf("Failed to get tx opts: %w", err)
+			return err
+		}
+
+		incredibleTask := taskmanager.IIncredibleDotProductTaskManagerTask{
+			PointsToMultiply:          taskmanager.IIncredibleDotProductTaskManagerDotProductInput{XAxis: task.InputValue.X, YAxis: task.InputValue.Y},
+			TaskCreatedBlock:          task.TaskCreatedBlock,
+			QuorumNumbers:             task.QuorumNumbers,
+			QuorumThresholdPercentage: task.QuorumThresholdPercentage,
+		}
+
+		incredibleTaskResponse := taskmanager.IIncredibleDotProductTaskManagerTaskResponse{
+			ReferenceTaskIndex: taskResponse.TaskResponse.ReferenceTaskIndex,
+			Result:             taskResponse.TaskResponse.OutputValue.Result,
+		}
+
+		incredibleTaskResponseMetadata := taskmanager.IIncredibleDotProductTaskManagerTaskResponseMetadata{
+			TaskRespondedBlock: taskResponse.TaskResponseMetadata.TaskRespondedBlock,
+			HashOfNonSigners:   taskResponse.TaskResponseMetadata.HashOfNonSigners,
+		}
+
+		pubkeysOfNonSigningOperators := []taskmanager.BN254G1Point{}
+		for i, pubkey := range taskResponse.NonSigningOperatorPubKeys {
+			pubkeysOfNonSigningOperators[i] = taskmanager.BN254G1Point{
+				X: pubkey.X,
+				Y: pubkey.Y,
+			}
+		}
+
+		tx, err := cv.taskManagerContract.RaiseAndResolveChallenge(noSendTxOpts, incredibleTask, incredibleTaskResponse, incredibleTaskResponseMetadata, pubkeysOfNonSigningOperators)
+		if err != nil {
+			cv.logger.Errorf("Failed to create raise and resolve challenge tx: %w", err)
+			return err
+		}
+
+		receipt, err := cv.txMgr.Send(context.Background(), tx, true)
+		if err != nil {
+			cv.logger.Errorf("Failed to send raise and resolve challenge tx: %w", err)
+			return err
+		}
+		if receipt.Status != uint64(1) {
+			return errors.New("failed to send raise and resolve challenge tx: receipt status was not success")
+		}
 
 	}
 
