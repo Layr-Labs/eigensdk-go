86fb299b475caf06f96a78da7aa883a663e055c7
maximopalopoli <mpalopoli@fi.uba.ar>
2025-05-06 16:04:40 -0300

    Use deployed addresses for task spammer

--- examples/incredible-dot-product/task-spammer/main.go
+++ examples/incredible-dot-product/task-spammer/main.go
@@ -1,4 +1,4 @@
-package taskspammer
+package main
 
 import (
 	"context"
@@ -6,10 +6,13 @@ import (
 	"math/big"
 	"time"
 
+	"github.com/Layr-Labs/eigensdk-go/chainio/clients/wallet"
 	"github.com/Layr-Labs/eigensdk-go/chainio/txmgr"
 	"github.com/Layr-Labs/eigensdk-go/logging"
+	"github.com/Layr-Labs/eigensdk-go/signerv2"
 	taskspammer "github.com/Layr-Labs/eigensdk-go/task-spammer"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 
 	taskmanager "github.com/Layr-Labs/eigensdk-go/examples/incredible-dot-product/contracts/bindings/IncredibleDotProductTaskManager"
@@ -21,7 +24,7 @@ type DotProductInput struct {
 }
 
 func main() {
-	logger, err := logging.NewZapLogger(logging.Development)
+	logger, err := logging.NewZapLogger(logging.Production)
 	if err != nil {
 		panic(err)
 	}
@@ -31,15 +34,38 @@ func main() {
 		logger.Errorf("Failed to get task manager abi: %w", err)
 	}
 
-	ethHttpUrl := "127.0.0.1:8545"
+	ethHttpUrl := "http://localhost:8545"
 	ethClient, err := ethclient.Dial(ethHttpUrl)
 	if err != nil {
 		logger.Errorf("Failed to dial ethclient: %w", err)
 	}
 
-	taskManagerAddr := common.HexToAddress("0x")
+	taskManagerAddr := common.HexToAddress("0x2bdcc0de6be1f7d2ee689a0342d76f52e8efaba3")
 
-	taskCreator, err := taskspammer.NewTaskCreatorFromAbi[DotProductInput](taskManagerAddr, *taskManagerAbi, &txmgr.SimpleTxManager{}, ethClient)
+	taskSpammerPrivateKey := "4bbbf85ce3377467afe5d46f804f221813b2bb87f24d81f60f1fcdbf7cbf4356"
+	ecdsaPrivateKey, err := crypto.HexToECDSA(taskSpammerPrivateKey)
+	if err != nil {
+		logger.Errorf("Failed to create ecdsa private key: %w", err)
+	}
+
+	chainId, err := ethClient.ChainID(context.Background())
+	if err != nil {
+		logger.Error("Cannot get chainId", "err", err)
+	}
+
+	taskSpammerAddr := common.HexToAddress("0x14dC79964da2C08b23698B3D3cc7Ca32193d9955")
+
+	signerV2, _, err := signerv2.SignerFromConfig(signerv2.Config{PrivateKey: ecdsaPrivateKey}, chainId)
+	if err != nil {
+		panic(err)
+	}
+	skWallet, err := wallet.NewPrivateKeyWallet(ethClient, signerV2, taskSpammerAddr, logger)
+	if err != nil {
+		panic(err)
+	}
+	txMgr := txmgr.NewSimpleTxManager(skWallet, ethClient, logger, taskSpammerAddr)
+
+	taskCreator, err := taskspammer.NewTaskCreatorFromAbi[DotProductInput](taskManagerAddr, *taskManagerAbi, txMgr, ethClient)
 	if err != nil {
 		logger.Errorf("Failed to create Task Creator: %w", err)
 	}
