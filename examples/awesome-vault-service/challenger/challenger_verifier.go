93ef31f7e429e74ca5eeb1c28a822c50b8676ba1
Tomás Grüner <47506558+MegaRedHand@users.noreply.github.com>
2025-05-12 11:29:29 -0300

    chore: copy incredible-dot-product example

---
+++ examples/awesome-vault-service/challenger/challenger_verifier.go
@@ -0,0 +1,138 @@
+package challenger
+
+import (
+	"context"
+	"math/big"
+
+	"github.com/Layr-Labs/eigensdk-go/chainio/txmgr"
+	"github.com/Layr-Labs/eigensdk-go/challenger"
+	"github.com/Layr-Labs/eigensdk-go/logging"
+	sdktypes "github.com/Layr-Labs/eigensdk-go/types"
+	"github.com/Layr-Labs/eigensdk-go/utils"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/ethclient"
+
+	delegationmanager "github.com/Layr-Labs/eigensdk-go/contracts/bindings/DelegationManager"
+	examplecommon "github.com/Layr-Labs/eigensdk-go/examples/incredible-dot-product/common"
+	taskmanager "github.com/Layr-Labs/eigensdk-go/examples/incredible-dot-product/contracts/bindings/IncredibleDotProductTaskManager"
+)
+
+type ChallengeVerifier struct {
+	logger                    logging.Logger
+	taskManagerContract       *taskmanager.ContractIncredibleDotProductTaskManager
+	txMgr                     txmgr.TxManager
+	delegationManagerContract *delegationmanager.ContractDelegationManager
+}
+
+func NewChallengeVerifier(
+	logger logging.Logger,
+	taskManagerAddr common.Address,
+	ethclient ethclient.Client,
+	txMgr txmgr.TxManager,
+	delegationManagerAddr common.Address,
+) (ChallengeVerifier, error) {
+	// Get the task manager contract with the given address
+	taskManagerContract, err := taskmanager.NewContractIncredibleDotProductTaskManager(taskManagerAddr, &ethclient)
+	if err != nil {
+		logger.Errorf("Failed to get Task Manager Contract: %w", err)
+		return ChallengeVerifier{}, err
+	}
+
+	delegationManagerContract, err := delegationmanager.NewContractDelegationManager(delegationManagerAddr, &ethclient)
+	if err != nil {
+		logger.Errorf("Failed to get delegation Manager Contract: %w", err)
+		return ChallengeVerifier{}, err
+	}
+
+	return ChallengeVerifier{
+		logger:                    logger,
+		taskManagerContract:       taskManagerContract,
+		txMgr:                     txMgr,
+		delegationManagerContract: delegationManagerContract,
+	}, nil
+}
+
+var _ challenger.ChallengeVerifier[examplecommon.DotProductInput, *big.Int] = (*ChallengeVerifier)(nil)
+
+func (cv ChallengeVerifier) VerifyChallenge(taskIndex uint32, task sdktypes.GenericInputTask[examplecommon.DotProductInput], taskResponse sdktypes.TaskResponseData[*big.Int]) error {
+	// Calculate response
+	totalSum, err := examplecommon.DotProduct(taskIndex, task.InputValue)
+	if err != nil {
+		cv.logger.Errorf("Failed to calculate task response: %w", err)
+		return err
+	}
+
+	// Compare submitted response with calculated here
+	receivedResponse := taskResponse.TaskResponse.OutputValue
+	shouldRaiseChallenge := totalSum.Cmp(receivedResponse) != 0
+
+	if shouldRaiseChallenge {
+		cv.logger.Infof("Response was not correct, expected %v and got %v", totalSum, receivedResponse)
+		// Call the Raise Challenge method of the on-chain contract
+
+		noSendTxOpts, err := cv.txMgr.GetNoSendTxOpts()
+		if err != nil {
+			cv.logger.Errorf("Failed to get tx opts: %w", err)
+			return err
+		}
+
+		incredibleTask := taskmanager.IIncredibleDotProductTaskManagerTask{
+			PointsToMultiply:          taskmanager.IIncredibleDotProductTaskManagerDotProductInput{X: task.InputValue.X, Y: task.InputValue.Y},
+			TaskCreatedBlock:          task.TaskCreatedBlock,
+			QuorumNumbers:             task.QuorumNumbers,
+			QuorumThresholdPercentage: task.QuorumThresholdPercentage,
+		}
+
+		incredibleTaskResponse := taskmanager.IIncredibleDotProductTaskManagerTaskResponse{
+			ReferenceTaskIndex: taskResponse.TaskResponse.ReferenceTaskIndex,
+			Result:             taskResponse.TaskResponse.OutputValue,
+		}
+
+		incredibleTaskResponseMetadata := taskmanager.IIncredibleDotProductTaskManagerTaskResponseMetadata{
+			TaskRespondedBlock: taskResponse.TaskResponseMetadata.TaskRespondedBlock,
+			HashOfNonSigners:   taskResponse.TaskResponseMetadata.HashOfNonSigners,
+		}
+
+		pubkeysOfNonSigningOperators := []taskmanager.BN254G1Point{}
+		for i, pubkey := range taskResponse.NonSigningOperatorPubKeys {
+			pubkeysOfNonSigningOperators[i] = taskmanager.BN254G1Point{
+				X: pubkey.X,
+				Y: pubkey.Y,
+			}
+		}
+
+		tx, err := cv.taskManagerContract.RaiseAndResolveChallenge(noSendTxOpts, incredibleTask, incredibleTaskResponse, incredibleTaskResponseMetadata, pubkeysOfNonSigningOperators)
+		if err != nil {
+			cv.logger.Errorf("Failed to create raise and resolve challenge tx: %w", err)
+			return err
+		}
+
+		receipt, err := cv.txMgr.Send(context.Background(), tx, true)
+		if err != nil {
+			cv.logger.Errorf("Failed to send raise and resolve challenge tx: %w", err)
+			return err
+		}
+		if receipt.Status != types.ReceiptStatusSuccessful {
+			cv.logger.Error("receipt status was not success sending raise challenge tx")
+			return utils.WrapError(err, "receipt status was not success")
+		}
+
+		// Note: this printing logic is not necessary, but left here to show how the operator shares
+		// at strategy decreases after raising a challenge (because it's being slashed)
+		shares, err := cv.delegationManagerContract.OperatorShares(
+			&bind.CallOpts{},
+			common.HexToAddress("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"), // Operator address
+			common.HexToAddress("0x2b961e3959b79326a8e7f64ef0d2d825707669b5"), // strategy address
+		)
+		if err != nil {
+			cv.logger.Errorf("Failed to get operator shares. Err: %w", err)
+			return err
+		}
+
+		cv.logger.Infof("After raising challenge, operator shares are %v", shares)
+	}
+
+	return nil
+}
---
+++ examples/awesome-vault-service/operator/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"context"
+	"math/big"
+
+	"github.com/Layr-Labs/eigensdk-go/logging"
+	"github.com/Layr-Labs/eigensdk-go/operator"
+
+	examplecommon "github.com/Layr-Labs/eigensdk-go/examples/incredible-dot-product/common"
+	taskmanager "github.com/Layr-Labs/eigensdk-go/examples/incredible-dot-product/contracts/bindings/IncredibleDotProductTaskManager"
+)
+
+func main() {
+	logger, err := logging.NewZapLogger(logging.Production)
+	if err != nil {
+		println("Failure creating logger")
+		return
+	}
+
+	taskManagerAbi, err := taskmanager.ContractIncredibleDotProductTaskManagerMetaData.GetAbi()
+	if err != nil {
+		logger.Errorf("Failed to get task manager abi: %w", err)
+		return
+	}
+
+	ethHttpUrl := "http://localhost:8545"
+
+	operatorAddr := "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+
+	operatorConfig := operator.OperatorConfig{
+		Logger:         logger,
+		TaskManagerAbi: taskManagerAbi,
+
+		OperatorAddress: operatorAddr,
+
+		AVSRegistryCoordinatorAddress: "0x7bc06c482dead17c0e297afbc32f6e63d3846650",
+		OperatorStateRetrieverAddress: "0x4c5859f0f772848b2d91f1d83e2fe57935348029",
+		ServiceManagerAddress:         "0x5f3f1dbd7b74c6b46e8c44f98792a1daf8d69154",
+
+		EthWsUrl:                      "ws://localhost:8545",
+		EthRpcUrl:                     ethHttpUrl,
+		AggregatorServerIpPortAddress: "localhost:8090",
+
+		BlsPrivateKeyStorePath: "keys/test.bls.key.json",
+	}
+
+	err = RegisterOperatorOnStartup(logger)
+	if err != nil {
+		logger.Fatalf("Failed to register operator on startup: %v", err.Error())
+	}
+
+	possibleFailureFunction, err := operator.ComputeWithFailures(examplecommon.DotProduct, failingDotProduct, 50)
+	if err != nil {
+		logger.Fatalf("Failed to create the possible failure function: %v", err.Error())
+	}
+
+	// Setting the TaskResponseHashFn parameter in nil because I'm using the abi default encoding function
+	operator, err := operator.NewOperatorFromConfig(operatorConfig, possibleFailureFunction, nil)
+	if err != nil {
+		logger.Fatalf("Failed to create operator: %w", err)
+	}
+
+	err = operator.Start(context.Background())
+	if err != nil {
+		logger.Fatalf("Failure while running operator: %w", err)
+	}
+}
+
+func failingDotProduct(taskIndex uint32, points examplecommon.DotProductInput) (*big.Int, error) {
+	return big.NewInt(31234213443), nil
+}
