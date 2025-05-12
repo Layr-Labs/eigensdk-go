c90b33e10055395bdbe28063918387a077da6388
maximopalopoli <mpalopoli@fi.uba.ar>
2025-05-12 15:36:45 -0300

    Remove BytesLib from awesome service manager contract

--- examples/awesome-vault-service/contracts/src/AwesomeVaultServiceManager.sol
+++ examples/awesome-vault-service/contracts/src/AwesomeVaultServiceManager.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-import "@eigenlayer/contracts/libraries/BytesLib.sol";
 import "./IAwesomeVaultTaskManager.sol";
 import "@eigenlayer-middleware/src/ServiceManagerBase.sol";
 import {
@@ -18,8 +17,6 @@ import {ISlashingRegistryCoordinator} from
  * @author Layr Labs, Inc.
  */
 contract AwesomeVaultServiceManager is ServiceManagerBase {
-    using BytesLib for bytes;
-
     IAwesomeVaultTaskManager public immutable awesomeVaultTaskManager;
 
     /// @notice when applied to a function, ensures that the function is only callable by the `registryCoordinator`.
