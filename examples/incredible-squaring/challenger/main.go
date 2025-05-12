8e9109bb17b85ca8a11991809020bfb23c4022fe
maximopalopoli <mpalopoli@fi.uba.ar>
2025-05-12 12:09:13 -0300

    Update incredible squaring challenger use example

--- examples/incredible-squaring/challenger/main.go
+++ examples/incredible-squaring/challenger/main.go
@@ -2,17 +2,14 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"math/big"
 
 	"github.com/Layr-Labs/eigensdk-go/chainio/txmgr"
 	"github.com/Layr-Labs/eigensdk-go/challenger"
 	"github.com/Layr-Labs/eigensdk-go/logging"
 	"github.com/Layr-Labs/eigensdk-go/testutils"
-	sdktypes "github.com/Layr-Labs/eigensdk-go/types"
 	"github.com/Layr-Labs/eigensdk-go/utils"
 	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 
@@ -56,13 +53,18 @@ func main() {
 		return
 	}
 
-	avsConfig := AvsConfig{
-		Logger:                        logger,
-		IncredibleSquaringTaskManager: common.HexToAddress("0x2bdcc0de6be1f7d2ee689a0342d76f52e8efaba3"),
-		TxMgr:                         txMgr,
-		EthHttpClient:                 ethHttpClient,
+	challengerRaiser, err := challenger.NewChallengerRaiserFromAbi[*big.Int, *big.Int](
+		common.HexToAddress("0x2bdcc0de6be1f7d2ee689a0342d76f52e8efaba3"),
+		taskManagerAbi,
+		txMgr,
+		ethHttpClient,
+	)
+	if err != nil {
+		logger.Errorf("Failed to create challenger raiser: %v", err)
+		return
 	}
-	challengeVerifierImpl, err := NewChallengeVerifierImpl(&avsConfig)
+
+	challengeVerifierImpl, err := challenger.NewIndexingChallengerProcessor(logger, squareValidation, challengerRaiser)
 	if err != nil {
 		logger.Errorf("Failed to create challenger logic from config: %v", err)
 		return
@@ -84,160 +86,17 @@ func main() {
 	}
 }
 
-// Challenger Logic
+func square(taskIndex uint32, numberToSquare *big.Int) (*big.Int, error) {
+	numberSquared := big.NewInt(0).Exp(numberToSquare, big.NewInt(2), nil)
 
-type ChallengeVerifierImpl struct {
-	logger    logging.Logger
-	ethClient *ethclient.Client
-	avsWriter *AvsWriter
+	return numberSquared, nil
 }
 
-var _ challenger.ChallengeVerifier[*big.Int, *big.Int] = (*ChallengeVerifierImpl)(nil)
-
-type TaskResponseData struct {
-	TaskResponse              cstaskmanager.IIncredibleSquaringTaskManagerTaskResponse
-	TaskResponseMetadata      cstaskmanager.IIncredibleSquaringTaskManagerTaskResponseMetadata
-	NonSigningOperatorPubKeys []cstaskmanager.BN254G1Point
-}
-
-func NewChallengeVerifierImpl(c *AvsConfig) (*ChallengeVerifierImpl, error) {
-	avsWriter, err := BuildAvsWriterFromConfig(c)
-	if err != nil {
-		c.Logger.Errorf("Cannot create avsWriter", "err", err)
-		return nil, err
+func squareValidation(taskIndex uint32, numberToSquare *big.Int, numberSquared *big.Int) (bool, error) {
+	result, err := square(taskIndex, numberToSquare)
+	if err != nil{
+		return false, utils.WrapError("failed to calculate square", err)
 	}
 
-	return &ChallengeVerifierImpl{
-		logger:    c.Logger,
-		ethClient: c.EthHttpClient,
-		avsWriter: avsWriter,
-	}, nil
-}
-
-func (c *ChallengeVerifierImpl) VerifyChallenge(
-	taskIndex uint32,
-	task sdktypes.GenericInputTask[*big.Int],
-	responseData sdktypes.TaskResponseData[*big.Int],
-) error {
-	nonSignerPubkeys := []cstaskmanager.BN254G1Point{}
-	for i, pubkey := range responseData.NonSigningOperatorPubKeys {
-		nonSignerPubkeys[i] = cstaskmanager.BN254G1Point{
-			X: pubkey.X,
-			Y: pubkey.Y,
-		}
-	}
-
-	numberToBeSquared := task.InputValue
-	answerInResponse := responseData.TaskResponse.OutputValue
-	trueAnswer := numberToBeSquared.Exp(numberToBeSquared, big.NewInt(2), nil)
-
-	// checking if the answer in the response submitted by aggregator is correct
-	if trueAnswer.Cmp(answerInResponse) != 0 {
-		c.logger.Info("The number squared is not correct", "expectedAnswer", trueAnswer, "gotAnswer", answerInResponse)
-
-		// raise challenge
-		c.logger.Info("Challenger raising challenge.", "taskIndex", taskIndex)
-
-		// This conversions are not optimal, but are necessary to send the challenge to the contract
-		incredibleSquaringTask := cstaskmanager.IIncredibleSquaringTaskManagerTask{
-			NumberToBeSquared:         task.InputValue,
-			TaskCreatedBlock:          task.TaskCreatedBlock,
-			QuorumNumbers:             task.QuorumNumbers,
-			QuorumThresholdPercentage: task.QuorumThresholdPercentage,
-		}
-
-		incredibleSquaringTaskResponse := cstaskmanager.IIncredibleSquaringTaskManagerTaskResponse{
-			ReferenceTaskIndex: responseData.TaskResponse.ReferenceTaskIndex,
-			NumberSquared:      responseData.TaskResponse.OutputValue,
-		}
-
-		incredibleSquaringTaskResponseMetadata := cstaskmanager.IIncredibleSquaringTaskManagerTaskResponseMetadata{
-			TaskRespondedBlock: responseData.TaskResponseMetadata.TaskRespondedBlock,
-			HashOfNonSigners:   responseData.TaskResponseMetadata.HashOfNonSigners,
-		}
-
-		_, err := c.avsWriter.RaiseChallenge(
-			context.Background(),
-			incredibleSquaringTask,
-			incredibleSquaringTaskResponse,
-			incredibleSquaringTaskResponseMetadata,
-			nonSignerPubkeys,
-		)
-		if err != nil {
-			c.logger.Error("Challenger failed to raise challenge:", "err", err)
-			return fmt.Errorf("challenger failed to raise challenge: %w", err)
-		}
-
-		return nil
-	} else {
-		c.logger.Info("The number squared is correct")
-		return nil
-	}
-}
-
-// Avs Writer
-type AvsConfig struct {
-	Logger                        logging.Logger
-	IncredibleSquaringTaskManager common.Address
-	TxMgr                         txmgr.TxManager
-	EthHttpClient                 *ethclient.Client
-}
-
-type AvsWriter struct {
-	logger              logging.Logger
-	TxMgr               txmgr.TxManager
-	taskManagerContract *cstaskmanager.ContractIncredibleSquaringTaskManager
-}
-
-func BuildAvsWriterFromConfig(c *AvsConfig) (*AvsWriter, error) {
-	contractTaskManager, err := cstaskmanager.NewContractIncredibleSquaringTaskManager(
-		c.IncredibleSquaringTaskManager,
-		c.EthHttpClient,
-	)
-	if err != nil {
-		return nil, utils.WrapError("Failed to fetch task manager contract", err)
-	}
-
-	return &AvsWriter{
-		logger:              c.Logger,
-		TxMgr:               c.TxMgr,
-		taskManagerContract: contractTaskManager,
-	}, nil
-}
-
-func (w *AvsWriter) RaiseChallenge(
-	ctx context.Context,
-	task cstaskmanager.IIncredibleSquaringTaskManagerTask,
-	taskResponse cstaskmanager.IIncredibleSquaringTaskManagerTaskResponse,
-	taskResponseMetadata cstaskmanager.IIncredibleSquaringTaskManagerTaskResponseMetadata,
-	pubkeysOfNonSigningOperators []cstaskmanager.BN254G1Point,
-) (*types.Receipt, error) {
-	txOpts, err := w.TxMgr.GetNoSendTxOpts()
-	if err != nil {
-		w.logger.Errorf("Error getting tx opts")
-		return nil, fmt.Errorf("error getting tx opts: %w", err)
-	}
-	tx, err := w.taskManagerContract.RaiseAndResolveChallenge(
-		txOpts,
-		task,
-		taskResponse,
-		taskResponseMetadata,
-		pubkeysOfNonSigningOperators,
-	)
-	if err != nil {
-		w.logger.Errorf("Error assembling RaiseChallenge tx")
-		return nil, fmt.Errorf("error assembling RaiseChallenge tx: %w", err)
-	}
-	receipt, err := w.TxMgr.Send(ctx, tx, true)
-	if err != nil {
-		w.logger.Errorf("Error submitting RaiseChallenge tx")
-		return nil, fmt.Errorf("error submitting RaiseChallenge tx: %w", err)
-	}
-	return receipt, nil
-}
-
-func (w *AvsWriter) ParseTaskResponded(
-	rawLog types.Log,
-) (*cstaskmanager.ContractIncredibleSquaringTaskManagerTaskResponded, error) {
-	return w.taskManagerContract.ContractIncredibleSquaringTaskManagerFilterer.ParseTaskResponded(rawLog)
+	return result.Cmp(numberSquared) == 0, nil
 }
