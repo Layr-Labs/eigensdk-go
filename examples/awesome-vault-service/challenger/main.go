2de2717ddeec2eaa8643a177c0b900ec5973856a
maximopalopoli <mpalopoli@fi.uba.ar>
2025-05-13 09:34:01 -0300

    Update vault service example after changes on challengre from v2-dev-1

--- examples/awesome-vault-service/challenger/main.go
+++ examples/awesome-vault-service/challenger/main.go
@@ -5,12 +5,14 @@ import (
 
 	"github.com/Layr-Labs/eigensdk-go/chainio/txmgr"
 	"github.com/Layr-Labs/eigensdk-go/challenger"
-	examplechallenger "github.com/Layr-Labs/eigensdk-go/examples/awesome-vault-service/challenger"
+	challengerprocessor "github.com/Layr-Labs/eigensdk-go/challenger/challenger-processor"
 	"github.com/Layr-Labs/eigensdk-go/logging"
+	"github.com/Layr-Labs/eigensdk-go/utils"
 	gethcommon "github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 
+	examplecommon "github.com/Layr-Labs/eigensdk-go/examples/awesome-vault-service/common"
 	taskmanager "github.com/Layr-Labs/eigensdk-go/examples/awesome-vault-service/contracts/bindings/AwesomeVaultTaskManager"
 )
 
@@ -49,8 +51,13 @@ func main() {
 		return
 	}
 
-	delegationManagerAddr := gethcommon.HexToAddress("0x9fe46736679d2d9a65f0992f2272de9f3c7fa6e0")
-	challengerVerifier, err := examplechallenger.NewChallengeVerifier(logger, taskManagerAddr, *ethClient, txMgr, delegationManagerAddr)
+	challengerRaiser, err := challengerprocessor.NewChallengeRaiserFromAbi[examplecommon.TaskInput, [32]byte](taskManagerAddr, taskManagerAbi, txMgr, ethClient)
+	if err != nil {
+		logger.Errorf("Failed to create challenger raiser: %w", err)
+		return
+	}
+
+	challengerProcessor, err := challengerprocessor.NewIndexingChallengerProcessor(logger, vaultSetValidation, challengerRaiser)
 	if err != nil {
 		logger.Errorf("Failed to create challenger verifier: %w", err)
 		return
@@ -62,7 +69,7 @@ func main() {
 		EthClient:      ethClient,
 		EthWsUrl:       "ws://localhost:8545",
 	}
-	challenger, err := challenger.NewChallenger(challengerConfig, challengerVerifier)
+	challenger, err := challenger.NewChallenger(challengerConfig, challengerProcessor)
 	if err != nil {
 		logger.Errorf("Failed to create challenger: %w", err)
 		return
@@ -73,4 +80,13 @@ func main() {
 		logger.Errorf("Failure while running challenger: %w", err)
 		return
 	}
+}
+
+func vaultSetValidation(taskIndex uint32, taskInput examplecommon.TaskInput, expectedOutput [32]byte) (bool, error) {
+	result, err := examplecommon.VaultSet(taskIndex, taskInput)
+	if err != nil {
+		return false, utils.WrapError("failed to set in the vault", err)
+	}
+
+	return result == expectedOutput, nil
 }
