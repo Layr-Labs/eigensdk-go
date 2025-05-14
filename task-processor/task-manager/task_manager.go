2ce2c73db8ea9707d616b7057d6bd761f5c1e28f
maximopalopoli <mpalopoli@fi.uba.ar>
2025-05-14 12:27:53 -0300

    Add comments for single interfaces explaining what they do

--- task-processor/task-manager/task_manager.go
+++ task-processor/task-manager/task_manager.go
@@ -23,11 +23,21 @@ type TaskCreator[Input any] interface {
 	CreateNewTask(ctx context.Context, input Input) error
 }
 
+// Interface for responding new tasks.
+//
+// Interface is generic over the task input and output types.
 type TaskResponder[Input any, Output any] interface {
+	// Saves the task response for a given task.
+	// Implementations usually submits to the on chain task manager contract the response to a task.
 	RespondToTask(task types.GenericInputTask[Input], taskResponse types.GenericOutputTaskResponse[Output], nonSignersStakesAndSig types.NonSignerStakesAndSignature) error
 	ProcessTaskResponse(taskResponse types.GenericOutputTaskResponse[Output]) (types.TaskResponseDigest, error)
 }
 
+// Interface for raising challenges for responded tasks.
+//
+// Interface is generic over the task input and output types.
 type ChallengeRaiser[Input any, Output any] interface {
+	// Raises a challenge for a responded task.
+	// Implementations usually sends the challenge to the on chain task manager contract.
 	RaiseChallenge(task types.GenericInputTask[Input], taskResponse types.GenericOutputTaskResponse[Output], TaskResponseMetadata types.GenericTaskResponseMetadata, NonSigningOperatorPubKeys []types.BN254G1Point) error
 }
