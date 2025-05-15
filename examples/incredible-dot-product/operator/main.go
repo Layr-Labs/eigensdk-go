140ee9d3b1eafde6b34ae45dd1b389176bd08e17
maximopalopoli <mpalopoli@fi.uba.ar>
2025-05-15 11:44:02 -0300

    Register operator in operator building instead of before

--- examples/incredible-dot-product/operator/main.go
+++ examples/incredible-dot-product/operator/main.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/Layr-Labs/eigensdk-go/logging"
 	"github.com/Layr-Labs/eigensdk-go/operator"
+	"github.com/ethereum/go-ethereum/common"
 
 	examplecommon "github.com/Layr-Labs/eigensdk-go/examples/incredible-dot-product/common"
 	taskmanager "github.com/Layr-Labs/eigensdk-go/examples/incredible-dot-product/contracts/bindings/IncredibleDotProductTaskManager"
@@ -28,6 +29,32 @@ func main() {
 
 	operatorAddr := "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
 
+	amount := new(big.Int)
+	amount.SetString("1000000000000000000000", 10)
+	registrationConfig := operator.RegistrationConfig{
+		RegisterOnStartup: true,
+
+		OperatorAddr:            common.HexToAddress("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"),
+		AllocationManagerAddr:   common.HexToAddress("0x2279b7a0a67db372996a5fab50d91eaa73d2ebe6"),
+		AvsAddress:              common.HexToAddress("0xcd8a1c3ba11cf5ecfa6267617243239504a98d90"),
+		RegistryCoordinatorAddr: common.HexToAddress("0xfd471836031dc5108809d173a067e8486b9047a3"),
+		StrategyAddr:            common.HexToAddress("0x2b961e3959b79326a8e7f64ef0d2d825707669b5"),
+
+		DelegationManagerAddress:    common.HexToAddress("0x9fe46736679d2d9a65f0992f2272de9f3c7fa6e0"),
+		RewardsCoordinatorAddress:   common.HexToAddress("0xa51c1fc2f0d1a1b8494ed1fe312d7c3a78ed91c0"),
+		PermissionControllerAddress: common.HexToAddress("0x59b670e9fa9d0a427751af201d676719a970857b"),
+
+		EthHttpUrl: "http://localhost:8545",
+
+		EcdsaKeyStorePath: "keys/test.ecdsa.key.json",
+		BlsKeyStorePath:   "keys/test.bls.key.json",
+
+		AmountToMint:         amount,
+		AllocatableMagnitude: 1000000000000000,
+
+		OperatorSetId: 0,
+	}
+
 	operatorConfig := operator.Config{
 		Logger:         logger,
 		TaskManagerAbi: taskManagerAbi,
@@ -43,11 +70,8 @@ func main() {
 		AggregatorServerIpPortAddress: "localhost:8090",
 
 		BlsPrivateKeyStorePath: "keys/test.bls.key.json",
-	}
 
-	err = RegisterOperatorOnStartup(logger)
-	if err != nil {
-		logger.Fatalf("Failed to register operator on startup: %v", err.Error())
+		RegistrationCfg: registrationConfig,
 	}
 
 	responseCalculator := operator.NewFunctionResponseCalculator(examplecommon.DotProduct)
