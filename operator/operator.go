5bac3176a70d7c73b08752dd9ba83f55bed8673b
maximopalopoli <mpalopoli@fi.uba.ar>
2025-05-20 13:07:06 -0300

    Add comment for TaskResponseHashFunction

--- operator/operator.go
+++ operator/operator.go
@@ -50,6 +50,7 @@ type Operator[Input any, Output any] struct {
 	taskResponseHashFn TaskResponseHashFunction[Output]
 }
 
+// The function used to hash the task responses, receiving the generic sdk task response and returning the digest
 type TaskResponseHashFunction[Output any] func(taskResponse taskmanager.TaskResponse[Output]) ([32]byte, error)
 
 // NewOperatorFromConfig creates a new Operator with the provided config and the functions to calculate and hashing the response.
