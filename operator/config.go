5e4d8718d1f287063ca4514bba36532b96abd892
maximopalopoli <mpalopoli@fi.uba.ar>
2025-05-20 09:52:56 -0300

    Add comments for the operator and registration configs

--- operator/config.go
+++ operator/config.go
@@ -9,45 +9,72 @@ import (
 )
 
 type Config struct {
+	// The address for the operator
 	OperatorAddress string
 
-	// Avs Reader addresses
+	// These addresses are used to initialize the avs registry chain reader
 	OperatorStateRetrieverAddress string
 	ServiceManagerAddress         string
 	AVSRegistryCoordinatorAddress string
 
+	// The url exposed by the anvil node to call the contract methods
 	EthRpcUrl string
-	EthWsUrl  string
 
-	BlsPrivateKeyStorePath        string
+	// The url exposed by the anvil node to listen to the contract events
+	EthWsUrl string
+
+	// The path to the location of the bls private key on local storage
+	BlsPrivateKeyStorePath string
+
+	// The address where the aggregator will listen for rpc calls from the operators
 	AggregatorServerIpPortAddress string
 
-	Logger         logging.Logger
+	Logger logging.Logger
+
+	// The abi of the task manager contract
 	TaskManagerAbi *abi.ABI
 
+	// The config used to register an operator
 	RegistrationCfg RegistrationConfig
 }
 
+// This config is used to register an operator on startup.
+// A TODO of this config is to make some values optional to perform some registration operations instead of all
 type RegistrationConfig struct {
+	// If set true, should register the operator on startup
 	RegisterOnStartup bool
 
-	OperatorAddr            common.Address
-	AllocationManagerAddr   common.Address
+	// The address for the operator
+	OperatorAddr common.Address
+
+	// Used for setting the allogation delay to zero and initialize allocations
+	AllocationManagerAddr common.Address
+
+	// Used to register operator in operator sets and initialize allocations
 	AvsAddress              common.Address
 	RegistryCoordinatorAddr common.Address
-	StrategyAddrs           []common.Address
 
+	// Used to deposit into these strategies for operator and initialize allocations on these strategies
+	StrategyAddrs []common.Address
+
+	// Used to create eigenlayer chain reader and writer
 	DelegationManagerAddress    common.Address
 	RewardsCoordinatorAddress   common.Address
 	PermissionControllerAddress common.Address
 
+	// The url exposed by the anvil node to call the contract methods
 	EthRpcUrl string
 
+	// The path to the location of the bls and ecdsa private keys on local storage
 	EcdsaKeyStorePath string
 	BlsKeyStorePath   string
 
-	AmountToMint          *big.Int
+	// The ammount to mint to the operator
+	AmountToMint *big.Int
+
+	// The magnitudes to be allocatable (slashable) in the strategies for the operator
 	AllocatableMagnitudes []uint64
 
+	// The IDs of the operator sets to be registered
 	OperatorSetIds []uint32
 }
