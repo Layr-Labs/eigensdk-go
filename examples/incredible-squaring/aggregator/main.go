d47d3c9f213dd0746c03d85af65e2327797d04e1
maximopalopoli <mpalopoli@fi.uba.ar>
2025-05-21 12:36:21 -0300

    Update the use examples

--- examples/incredible-squaring/aggregator/main.go
+++ examples/incredible-squaring/aggregator/main.go
@@ -49,10 +49,8 @@ func main() {
 	cfg := aggregator.Config{
 		RegistryCoordinatorAddress:    common.HexToAddress("0x7bc06c482dead17c0e297afbc32f6e63d3846650"),
 		OperatorStateRetrieverAddress: common.HexToAddress("0x4c5859f0f772848b2d91f1d83e2fe57935348029"),
-		ServiceManagerAddress:         common.HexToAddress("0x5f3f1dbd7b74c6b46e8c44f98792a1daf8d69154"),
 		EthHttpUrl:                    ethHttpUrl,
 		EthWsUrl:                      "ws://localhost:8545",
-		EcdsaPrivateKey:               ecdsaPrivateKey,
 		AggregatorServerIpPortAddr:    "localhost:8090",
 	}
 
