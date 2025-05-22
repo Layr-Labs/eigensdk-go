c0d388b15eeab3621757752669a973365b42524b
maximopalopoli <mpalopoli@fi.uba.ar>
2025-05-22 12:51:55 -0300

    Add comments on each step for inc squaring challenger

--- examples/incredible-squaring/challenger/main.go
+++ examples/incredible-squaring/challenger/main.go
@@ -21,23 +21,26 @@ import (
 )
 
 func main() {
+	// 1. Create the logger where all the loggs will appear
 	logger, err := logging.NewZapLogger(logging.Production) // Change here if want to change logging level
 	if err != nil {
 		println("Failure creating logger")
 		return
 	}
 
+	// 2. Get the ABI of the task manager contract's binding
 	taskManagerAbi, err := cstaskmanager.ContractIncredibleSquaringTaskManagerMetaData.GetAbi()
 	if err != nil {
 		logger.Fatalf(err.Error())
 	}
 
-	ethHttpUrl := "http://localhost:8545"
-	ethHttpClient, err := ethclient.Dial(ethHttpUrl)
+	// 3. Create the ethereum client that will send the RPC messages to the node
+	ethHttpClient, err := ethclient.Dial("http://localhost:8545")
 	if err != nil {
 		return
 	}
 
+	// 4. Create the transaction manager, that will manage the transaction sending
 	ecdsaPrivateKey, err := crypto.HexToECDSA(testutils.ANVIL_FIRST_PRIVATE_KEY)
 	if err != nil {
 		logger.Errorf("Cannot parse ecdsa private key", "err", err)
@@ -50,6 +53,11 @@ func main() {
 		return
 	}
 
+	// 5. Create the challenger raiser, which will raise the challenges to the on-chain TaskManager contract.
+	// Here we use an SDK implementation that satisfies the ChallengeRaiser interface, but you can create your own wrapper
+	// which implements the interface and provide it to the Indexing Task Processor.
+	// Note that in this step we define the input and output types that we are using on our AVS. In this case
+	// the input and output are both big int numbers.
 	challengerRaiser, err := taskmanager.NewTaskManagerFromAbi[*big.Int, *big.Int](
 		gethcommon.HexToAddress("0x2bdcc0de6be1f7d2ee689a0342d76f52e8efaba3"),
 		taskManagerAbi,
@@ -61,14 +69,24 @@ func main() {
 		return
 	}
 
+	// 6. Create the calculator and validation function with the AVS calculation logic. Note that here we create a
+	// Response calculator with the NewFunctionResponseCalculator from the operator package, and with that
+	// calculator we create the validator with the ResponseValidationFunctionFromResponseCalculator builder from
+	// the challengerprocessor package.
 	squareCalculator := operator.NewFunctionResponseCalculator(examplecommon.Square)
 	squareValidation := challengerprocessor.ResponseValidationFunctionFromResponseCalculator(squareCalculator, common.BigIntEqual)
+
+	// 7. Create the Challenger Processor, which will manage the challenge raising in case the response is not
+	// correct. Here we use the IndexingChallengerProcessor, a generic implementation provided by the SDK that
+	// saves the tasks in a map and in case of receiving a wrong response delegates the raising of the
+	// challenges to the on-chain TaskManager contract.
 	indexingTaskProcessor, err := challengerprocessor.NewIndexingChallengerProcessor(logger, squareValidation, challengerRaiser)
 	if err != nil {
 		logger.Errorf("Failed to create challenger logic from config: %v", err)
 		return
 	}
 
+	// 8. Create the config passed to the challenger.
 	cfg := challenger.Config{
 		EthWsUrl:       "ws://localhost:8545",
 		Logger:         logger,
@@ -76,6 +94,7 @@ func main() {
 		EthClient:      ethHttpClient,
 	}
 
+	// 9. Build the challenger, providing challenger config, and the challenger processor.
 	challenger, err := challenger.NewChallenger(
 		cfg,
 		indexingTaskProcessor,
@@ -85,6 +104,7 @@ func main() {
 		return
 	}
 
+	// 10. Run the created challenger
 	err = challenger.Start(context.Background())
 	if err != nil {
 		logger.Errorf("Error while running operator: %v", err)
