7c9a790f933b450e3be00a55b578fc3b239f0bee
maximopalopoli <mpalopoli@fi.uba.ar>
2025-05-22 17:23:10 -0300

    Rewrite the step comments to sync with the aggregator module on avs aggregator

--- examples/awesome-vault-service/aggregator/main.go
+++ examples/awesome-vault-service/aggregator/main.go
@@ -37,36 +37,33 @@ func GetConfigFromPath(path string) (*Config, error) {
 	return config, err
 }
 
+// This is the main function for the aggregator in the awesome vault service example. The steps followed are
+// also explained in the aggregator module readme, which can be found at aggregator/README.md
 func main() {
-	// 1. Create the logger where all the logs will appear
+	// 0. Create the logger where all the logs will appear
 	logger, err := logging.NewZapLogger(logging.Production)
 	if err != nil {
 		println("Failure creating logger")
 		return
 	}
 
-	// 2. Get the config from the aggregator config file, reading the required information to create the aggregator
+	// 1. Create the aggregator configuration (in this case we read it from aggregator config file)
 	config, err := GetConfigFromPath("config/aggregator_config.toml")
 	if err != nil {
 		logger.Errorf("Failed to read config file: %w", err)
 		return
 	}
+	aggConfig := config.Config
 
-	// 3. Get the ABI of the task manager contract's binding
-	taskManagerAbi, err := avtaskmanager.ContractAwesomeVaultTaskManagerMetaData.GetAbi()
-	if err != nil {
-		logger.Errorf("Failed to get task manager abi: %w", err)
-		return
-	}
+	// 2. Provide a Task Processor, first instantiating the things required for creating it
 
-	// 4. Create the ethereum client that will send the RPC messages to the node
+	// i. Create the ethereum client that will send the RPC messages to the node, and the transaction
+	// manager, that will manage the transaction sending
 	ethClient, err := ethclient.Dial(config.EthHttpUrl)
 	if err != nil {
 		logger.Errorf("Failed to dial ethclient: %w", err)
 		return
 	}
-
-	// 5. Create the transaction manager, that will manage the transaction sending
 
 	// Depends on the aggregatorPrivateKey passed to TaskManager.initialize
 	// To change it, modify aggregator_addr in
@@ -84,11 +81,16 @@ func main() {
 		return
 	}
 
-	aggConfig := config.Config
+	// ii. Get the ABI of the task manager contract's binding
+	taskManagerAbi, err := avtaskmanager.ContractAwesomeVaultTaskManagerMetaData.GetAbi()
+	if err != nil {
+		logger.Errorf("Failed to get task manager abi: %w", err)
+		return
+	}
 
-	// 6. Create the task responder, that will send the aggregated responses to the on-chain TaskManager contract.
-	// Here we use an SDK implementation that satisfies the TaskResponder interface, but you can create your wrapper
-	// (which implements the interface) and provide it to the Indexing Task Processor.
+	// iii. Provide a struct that implements the `TaskResponder` interface. Here we use an SDK implementation
+	// that satisfies the TaskResponder interface, but you can create your wrapper (which implements the
+	// interface) and provide it to the Indexing Task Processor.
 	// Note that in this step we define the input and output types that we are using on our AVS. In this case
 	// the TaskInput struct (a key-value pair) and 32 bytes.
 	taskManagerAddr := gethcommon.HexToAddress(config.TaskManagerAddress)
@@ -98,23 +100,22 @@ func main() {
 		return
 	}
 
-	// 7. Create the Task Processor, which will manage the processing of the tasks and the aggregated responses.
-	// Here we use the IndexingTaskProcessor, a generic implementation provided by the SDK that saves the tasks
-	// in a map and delegates the sending of aggregated responses to the on-chain TaskManager contract.
+	// iv. Create the Task Processor. Here we use the IndexingTaskProcessor, you can see its implementation
+	// in aggregator/task-processor/indexing_task_processor.go
 	taskProcessor, err := taskprocessor.NewIndexingTaskProcessor(logger, taskResponder)
 	if err != nil {
 		logger.Errorf("Failed to create Task Processor: %w", err)
 		return
 	}
 
-	// 8. Build the aggregator, providing aggregator config, logger, task processor and the task manager ABI.
+	// 3. Build the aggregator, providing aggregator config, logger, task processor and the task manager ABI, and
+	// then start it.
 	aggregator, err := aggregator.NewAggregator(aggConfig, logger, taskProcessor, taskManagerAbi)
 	if err != nil {
 		logger.Errorf("Failed to create aggregator: %w", err)
 		return
 	}
 
-	// 9. Run the created aggregator
 	err = aggregator.Start(context.Background())
 	if err != nil {
 		logger.Errorf("Failure while running aggregator: %w", err)
