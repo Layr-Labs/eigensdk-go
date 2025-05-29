9b2ff640f307664ba726daff7a0e27c1c0451f44
maximopalopoli <mpalopoli@fi.uba.ar>
2025-05-29 17:11:23 -0300

    Replace literal with readed config value

--- examples/incredible-squaring/aggregator/main.go
+++ examples/incredible-squaring/aggregator/main.go
@@ -81,7 +81,7 @@ func main() {
 	// that already satisfies it, but you can also provide your own type implementing the interface.
 	// Note that in this step we define the input and output types that we are using on our AVS. In this case
 	// the input and output are both big int numbers.
-	taskManagerAddr := common.HexToAddress("0x2bdcc0de6be1f7d2ee689a0342d76f52e8efaba3")
+	taskManagerAddr := common.HexToAddress(config.TaskManagerAddress)
 	taskResponder, err := taskmanager.NewTaskManagerFromAbi[*big.Int, *big.Int](
 		taskManagerAddr,
 		taskManagerAbi,
