2887f27ee04f4752cffc26f872f6ffe830fc8d9a
Maximo Palopoli <96491141+maximopalopoli@users.noreply.github.com>
2025-05-30 16:53:05 -0300

    feat(v2): add TOML tags to the operator config (#711)

--- operator/config.go
+++ operator/config.go
@@ -3,70 +3,62 @@ package operator
 import (
 	"math/big"
 
-	"github.com/Layr-Labs/eigensdk-go/logging"
-	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
 )
 
 // Operator configuration struct
 type Config struct {
 	// The address for the operator
-	OperatorAddress string
+	OperatorAddress string `toml:"operator_address"`
 
 	// The registry coordinator address is used to create the AVS reader
-	RegistryCoordinatorAddress string
+	RegistryCoordinatorAddress string `toml:"registry_coordinator_address"`
 
 	// Ethereum HTTP RPC URL to use for interacting with on-chain contracts
-	EthRpcUrl string
+	EthRpcUrl string `toml:"eth_http_url"`
 
 	// Ethereum WebSocket RPC URL to use for subscribing to on-chain events
-	EthWsUrl string
+	EthWsUrl string `toml:"eth_ws_url"`
 
 	// The path to the location of the bls private key on local storage
-	BlsPrivateKeyStorePath string
+	BlsPrivateKeyStorePath string `toml:"bls_private_key_store_path"`
 
 	// IP address and port where the aggregator will listen to operator task responses
-	AggregatorServerIpPortAddress string
-
-	// The logger to use when logging
-	Logger logging.Logger
-
-	// The ABI of the task manager contract
-	TaskManagerAbi *abi.ABI
+	AggregatorServerIpPortAddress string `toml:"aggregator_server_ip_port"`
 
 	// The config used to register an operator
-	RegistrationCfg RegistrationConfig
+	Registration RegistrationConfig `toml:"registration"`
 }
 
 // This config is used to register an operator on startup.
 // A TODO of this config is to make some values optional to perform some registration operations instead of all
 type RegistrationConfig struct {
 	// If set true, should register the operator on startup
-	RegisterOnStartup bool
+	RegisterOnStartup bool `toml:"register_on_startup"`
 
 	// Used for setting the allogation delay to zero and initialize allocations
-	AllocationManagerAddr common.Address
+	AllocationManagerAddr common.Address `toml:"allocation_manager_address"`
 
 	// Used to register operator in operator sets and initialize allocations
-	AvsAddress common.Address
+	AvsAddress common.Address `toml:"service_manager_address"`
 
 	// Used to deposit into these strategies for operator and initialize allocations on these strategies
-	StrategyAddrs []common.Address
+	StrategyAddrs []common.Address `toml:"strategy_addresses"`
 
 	// Used to create eigenlayer chain reader and writer
-	DelegationManagerAddress    common.Address
-	RewardsCoordinatorAddress   common.Address
-	PermissionControllerAddress common.Address
+	DelegationManagerAddress    common.Address `toml:"delegation_manager_address"`
+	RewardsCoordinatorAddress   common.Address `toml:"rewards_coordinator_address"`
+	PermissionControllerAddress common.Address `toml:"permission_controller_address"`
 
 	// The path to the location of the ecdsa private key on local storage
-	EcdsaKeyStorePath string
+	EcdsaKeyStorePath string `toml:"ecdsa_private_key_store_path"`
 
 	// The ammount to mint to the operator
-	AmountToMint *big.Int
+	AmountToMint *big.Int `toml:"amount_to_mint"`
 
 	// The magnitudes to be allocatable (slashable) in the strategies for the operator
-	AllocatableMagnitudes []uint64
+	AllocatableMagnitudes []uint64 `toml:"allocatable_magnitudes"`
 
 	// The IDs of the operator sets to be registered
-	OperatorSetIds []uint32
+	OperatorSetIds []uint32 `toml:"operator_set_ids"`
 }
