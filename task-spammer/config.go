6968c103a137f2355d64bcee712f8184eca75aad
maximopalopoli <mpalopoli@fi.uba.ar>
2025-05-30 17:44:05 -0300

    Add serialization tags to spammer config

--- task-spammer/config.go
+++ task-spammer/config.go
@@ -10,11 +10,11 @@ import (
 // TODO: have default values
 type Config struct {
 	// The duration of the period between tasks
-	TimeBetweenTasks time.Duration
+	TimeBetweenTasks time.Duration `toml:"time_between_tasks"`
 
 	// The percentage of the total quorum stake needed by the signers to make the aggregated response valid
-	QuorumThresholdPercentage uint32
+	QuorumThresholdPercentage uint32 `toml:"quorum_threshold_percentage"`
 
 	// The numbers of the quorums required to respond to tasks for the response to be valid
-	QuorumNumbers []uint8
+	QuorumNumbers []uint8 `toml:"quorum_number"`
 }
