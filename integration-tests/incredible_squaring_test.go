089844181e3d609b8d59132f619aa631902d9453
maximopalopoli <mpalopoli@fi.uba.ar>
2025-06-04 16:27:19 -0300

    Fix is integration test

--- integration-tests/incredible_squaring_test.go
+++ integration-tests/incredible_squaring_test.go
@@ -206,6 +206,11 @@ func createIncredibleSquaringOperator(t *testing.T, ethHttpUrl, ethWsUrl string)
 	taskManagerAbi, err := cstaskmanager.ContractIncredibleSquaringTaskManagerMetaData.GetAbi()
 	require.NoError(t, err, "Failed to get task manager abi")
 
+	ecdsaSignerCfg := operator.EcdsaSignerConfig{
+		KeystorePath:     "../examples/incredible-squaring/keys/test.ecdsa.key.json",
+		KeystorePassword: "",
+	}
+
 	amount := new(big.Int)
 	amount.SetString("1000000000000000000000", 10)
 	registrationConfig := operator.RegistrationConfig{
@@ -219,8 +224,7 @@ func createIncredibleSquaringOperator(t *testing.T, ethHttpUrl, ethWsUrl string)
 		RewardsCoordinatorAddress:   common.HexToAddress("0xa51c1fc2f0d1a1b8494ed1fe312d7c3a78ed91c0"),
 		PermissionControllerAddress: common.HexToAddress("0x59b670e9fa9d0a427751af201d676719a970857b"),
 
-		// Current dir is integration-tests
-		EcdsaKeyStorePath: "../examples/incredible-squaring/keys/test.ecdsa.key.json",
+		EcdsaSignerCfg: ecdsaSignerCfg,
 
 		AmountToMint:          amount,
 		AllocatableMagnitudes: []uint64{1000000000000000},
