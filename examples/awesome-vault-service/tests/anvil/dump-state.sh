e12d64e008807b24781d9c27b3ca4cb429157831
maximopalopoli <mpalopoli@fi.uba.ar>
2025-06-05 16:12:08 -0300

    update dump state

--- examples/awesome-vault-service/tests/anvil/dump-state.sh
+++ examples/awesome-vault-service/tests/anvil/dump-state.sh
@@ -34,7 +34,7 @@ DEPLOYER_PRIVATE_KEY=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf
 set +a
 
 # start an empty anvil chain in the background and dump its state to a json file upon exit
-start_anvil_docker "" $anvil_dir/incredible-dot-product-anvil-state.json
+start_anvil_docker "" $anvil_dir/awesome-vault-service-anvil-state.json
 sleep 1
 
 $root_dir/tests/anvil/deploy-eigenlayer.sh
