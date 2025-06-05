fdbbfb0b404467e8aa7991853e2cdbbec405abb5
maximopalopoli <mpalopoli@fi.uba.ar>
2025-06-05 11:33:36 -0300

    Add the generic functions to build the entities

--- integration-tests/avs_test_generator.go
+++ integration-tests/avs_test_generator.go
@@ -2,9 +2,23 @@ package integration_test
 
 import (
 	"iter"
+	"math/big"
+	"testing"
+	"time"
 
+	"github.com/Layr-Labs/eigensdk-go/aggregator"
+	"github.com/Layr-Labs/eigensdk-go/chainio/txmgr"
+	"github.com/Layr-Labs/eigensdk-go/challenger"
+	"github.com/Layr-Labs/eigensdk-go/logging"
+	"github.com/Layr-Labs/eigensdk-go/operator"
+	taskmanager "github.com/Layr-Labs/eigensdk-go/task-manager"
+	taskspammer "github.com/Layr-Labs/eigensdk-go/task-spammer"
+	"github.com/Layr-Labs/eigensdk-go/testutils"
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/stretchr/testify/require"
 )
 
 type TestConfig[Input any, Output any] struct {
@@ -39,4 +53,259 @@ type TestConfig[Input any, Output any] struct {
 
 	// Check if really needed
 	AggregatorServerIpPortAddr string
+}
+
+// func TestAvsExample[Input any, Output any](t *testing.T, config TestConfig[Input, Output]) {
+// 	ctx, cancel := context.WithCancel(context.Background())
+
+// 	anvilC, err := testutils.StartAnvilContainer("../../examples/incredible-squaring/tests/anvil/incredible-squaring-anvil-state.json")
+// 	require.NoError(t, err)
+// 	ethHttpUrl, err := anvilC.Endpoint(context.Background(), "http")
+// 	require.NoError(t, err)
+// 	ethWsUrl, err := anvilC.Endpoint(context.Background(), "ws")
+// 	require.NoError(t, err)
+
+// 	ethClient, err := ethclient.Dial(ethHttpUrl)
+// 	require.NoError(t, err, "Failed to create eth client")
+
+// 	taskManager, err := cstaskmanager.NewContractIncredibleSquaringTaskManager(taskManagerAddress, ethClient)
+// 	require.NoError(t, err, "Failed to create task manager contract")
+
+// 	initialTaskIndex, err := taskManager.TaskNumber(&bind.CallOpts{})
+// 	require.NoError(t, err, "Failed to get initial task index")
+// 	require.Equal(t, uint32(0), initialTaskIndex, "Initial task index should be 0")
+
+// 	// Aggregator
+// 	aggregator := createAvsAggregator(t, ethHttpUrl, ethWsUrl, config)
+
+// 	aggErrC := aggregator.Start(ctx)
+
+// 	// Challenger
+// 	challenger := createAvsChallenger(t, ethHttpUrl, ethWsUrl, config)
+
+// 	chErrC := challenger.Start(ctx)
+
+// 	// Operator
+// 	operator := createAvsOperator(t, ethHttpUrl, ethWsUrl, config)
+
+// 	opErrC := operator.Start(ctx)
+
+// 	// Task Spammer
+// 	taskSpammer := createAvsTaskSpammer(t, ethHttpUrl, config)
+
+// 	tsErrC := taskSpammer.Start(ctx)
+
+// 	timer := time.NewTimer(32 * time.Second)
+
+// 	select {
+// 	case err := <-aggErrC:
+// 		t.Fatal("Aggregator error:", err)
+// 	case err := <-chErrC:
+// 		t.Fatal("Challenger error:", err)
+// 	case err := <-opErrC:
+// 		t.Fatal("Operator error:", err)
+// 	case err := <-tsErrC:
+// 		// Here we handle the input generation termination (in that case returns nil but does not imply an error)
+// 		if err == nil {
+// 			cancel()
+// 		} else {
+// 			t.Fatal("Task Spammer error:", err)
+// 		}
+// 	case <-timer.C:
+// 		// If reached this point, there must be a problem with the tasks generation.
+// 		t.Fatal("Timer expired before the task spammer finished sending tasks")
+// 	}
+
+// 	taskIndex, err := taskManager.TaskNumber(&bind.CallOpts{})
+// 	require.NoError(t, err, "Failed to get final task index")
+// 	require.Equal(t, taskIndex, uint32(3), "Final task index should be equal to the amount of tasks generated")
+
+// 	for i := range uint32(3) {
+// 		taskResponse, err := taskManager.AllTaskResponses(&bind.CallOpts{}, i)
+// 		require.NoError(t, err, "Failed to get task response number %v", i)
+// 		require.NotZero(t, taskResponse, "Task response number %v should not be nil", i)
+// 	}
+// }
+
+func createAvsAggregator[Input any, Output any](t *testing.T, ethHttpUrl, ethWsUrl string, config TestConfig[Input, Output]) *aggregator.Aggregator[Input, Output] {
+	t.Helper()
+
+	logger, err := logging.NewZapLogger(logging.Production)
+	require.NoError(t, err, "Failure creating logger")
+
+	cfg := aggregator.Config{
+		RegistryCoordinatorAddress:    common.HexToAddress(config.RegistryCoordinatorAddress),
+		OperatorStateRetrieverAddress: config.OperatorStateRetrieverAddress,
+		EthHttpUrl:                    ethHttpUrl,
+		EthWsUrl:                      ethWsUrl,
+		AggregatorServerIpPortAddr:    config.AggregatorServerIpPortAddr,
+	}
+
+	ethClient, err := ethclient.Dial(ethHttpUrl)
+	require.NoError(t, err, "Failure creating ethclient")
+
+	aggregatorPrivateKey := "2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6"
+	ecdsaPrivateKey, err := crypto.HexToECDSA(aggregatorPrivateKey)
+	require.NoError(t, err, "Failed to create ecdsa private key")
+
+	txMgr, err := txmgr.NewSimpleTxManagerFromPrivateKey(logger, ethClient, ecdsaPrivateKey)
+	require.NoError(t, err, "Failed to create tx manager from private key")
+
+	taskResponder, err := taskmanager.NewTaskManagerFromAbi[Input, Output](
+		config.TaskManagerAddr,
+		config.TaskManagerAbi,
+		txMgr,
+		ethClient,
+	)
+	require.NoError(t, err, "Failed to create Task Responder")
+
+	aggregatorProcessor, err := aggregator.NewIndexingProcessor(logger, taskResponder)
+	require.NoError(t, err, "Failed to create Processor")
+
+	aggregator, err := aggregator.NewAggregator(logger, cfg, config.TaskManagerAbi, aggregatorProcessor)
+	require.NoError(t, err, "Failed to create aggregator")
+
+	return aggregator
+}
+
+func createAvsChallenger[Input any, Output any](
+	t *testing.T,
+	ethHttpUrl, ethWsUrl string,
+	config TestConfig[Input, Output],
+) *challenger.Challenger[Input, Output] {
+	t.Helper()
+
+	logger, err := logging.NewZapLogger(logging.Production)
+	require.NoError(t, err, "Failure creating logger")
+
+	ethHttpClient, err := ethclient.Dial(ethHttpUrl)
+	require.NoError(t, err, "Failed to create eth client")
+
+	challengerCfg := challenger.Config{
+		EthWsUrl:   ethWsUrl,
+		EthHttpUrl: ethHttpUrl,
+	}
+
+	logicCalculator := operator.NewFunctionResponseCalculator(config.LogicFn)
+	logicValidation := challenger.ResponseValidationFunctionFromResponseCalculator(logicCalculator, config.EqualFn)
+
+	ecdsaPrivateKey, err := crypto.HexToECDSA(testutils.ANVIL_FIRST_PRIVATE_KEY)
+	require.NoError(t, err, "Failed to parse ecdsa private key")
+
+	txMgr, err := txmgr.NewSimpleTxManagerFromPrivateKey(logger, ethHttpClient, ecdsaPrivateKey)
+	require.NoError(t, err, "Failed to create transaction manager")
+
+	challengeRaiser, err := taskmanager.NewTaskManagerFromAbi[Input, Output](
+		config.TaskManagerAddr,
+		config.TaskManagerAbi,
+		txMgr,
+		ethHttpClient,
+	)
+	require.NoError(t, err, "Failed to create challenge raiser")
+
+	challengerProcessor, err := challenger.NewIndexingProcessor(logger, logicValidation, challengeRaiser)
+	require.NoError(t, err, "Failed to create challenger processor")
+
+	challenger, err := challenger.NewChallenger(
+		logger,
+		challengerCfg,
+		config.TaskManagerAbi,
+		challengerProcessor,
+	)
+	require.NoError(t, err, "Failed to create challenger from config")
+
+	return challenger
+}
+
+func createAvsOperator[Input any, Output any](
+	t *testing.T,
+	ethHttpUrl, ethWsUrl string,
+	config TestConfig[Input, Output],
+) *operator.Operator[Input, Output] {
+	t.Helper()
+
+	logger, err := logging.NewZapLogger(logging.Production)
+	require.NoError(t, err, "Failure creating logger")
+
+	amount := new(big.Int)
+	amount.SetString("1000000000000000000000", 10)
+	registrationConfig := operator.RegistrationConfig{
+		RegisterOnStartup: true,
+
+		AllocationManagerAddr: common.HexToAddress("0x2279b7a0a67db372996a5fab50d91eaa73d2ebe6"),
+		AvsAddress:            config.AvsAddress,
+		StrategyAddrs:         []common.Address{common.HexToAddress("0x2b961e3959b79326a8e7f64ef0d2d825707669b5")},
+
+		DelegationManagerAddress:    common.HexToAddress("0x9fe46736679d2d9a65f0992f2272de9f3c7fa6e0"),
+		RewardsCoordinatorAddress:   common.HexToAddress("0xa51c1fc2f0d1a1b8494ed1fe312d7c3a78ed91c0"),
+		PermissionControllerAddress: common.HexToAddress("0x59b670e9fa9d0a427751af201d676719a970857b"),
+
+		EcdsaKeyStorePath: config.EcdsaKeyStorePath,
+
+		AmountToMint:          amount,
+		AllocatableMagnitudes: []uint64{1000000000000000},
+
+		OperatorSetIds: []uint32{0},
+
+		MetadataUrl:     "",
+		Socket:          "",
+		AllocationDelay: 0,
+	}
+
+	blsSignerConfig := operator.BlsSignerConfig{
+		KeystorePath:     config.BlsKeyStorePath,
+		KeystorePassword: "",
+	}
+	operatorConfig := operator.Config{
+		OperatorAddress:               "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+		RegistryCoordinatorAddress:    config.RegistryCoordinatorAddress,
+		EthRpcUrl:                     ethHttpUrl,
+		EthWsUrl:                      ethWsUrl,
+		BlsSignerCfg:                  blsSignerConfig,
+		AggregatorServerIpPortAddress: config.AggregatorServerIpPortAddr,
+		Registration:                  registrationConfig,
+	}
+
+	calculator := operator.NewFunctionResponseCalculator(config.LogicFn)
+
+	operator, err := operator.NewOperator(logger, operatorConfig, config.TaskManagerAbi, calculator, nil)
+	require.NoError(t, err, "Failed to create operator from config")
+
+	return operator
+}
+
+func createAvsTaskSpammer[Input any, Output any](
+	t *testing.T,
+	ethHttpUrl string,
+	config TestConfig[Input, Output],
+) *taskspammer.TaskSpammer[Input] {
+	t.Helper()
+
+	logger, err := logging.NewZapLogger(logging.Production)
+	require.NoError(t, err, "Failure creating logger")
+
+	ethHttpClient, err := ethclient.Dial(ethHttpUrl)
+	require.NoError(t, err, "Failed to dial ethclient")
+
+	ecdsaPrivateKey, err := crypto.HexToECDSA(config.TaskSpammerPrivateKey)
+	require.NoError(t, err, "Failed to create ecdsa private key")
+
+	txMgr, err := txmgr.NewSimpleTxManagerFromPrivateKey(logger, ethHttpClient, ecdsaPrivateKey)
+	require.NoError(t, err, "Failed to create transaction manager from private key")
+
+	taskCreator, err := taskmanager.NewTaskManagerFromAbi[Input, Output](config.TaskManagerAddr, config.TaskManagerAbi, txMgr, ethHttpClient)
+	require.NoError(t, err, "Failed to create Task Creator")
+
+	taskSpammerConfig := taskspammer.Config{
+		// This means TaskGenerator will send tasks every 10 seconds
+		TimeBetweenTasks: 10 * time.Second,
+
+		QuorumThresholdPercentage: 100,
+		QuorumNumbers:             []uint8{0},
+	}
+
+	taskSpammer, err := taskspammer.NewTaskSpammer(logger, taskSpammerConfig, taskCreator, config.InputSequence)
+	require.NoError(t, err, "Failed to create Task Spammer")
+
+	return taskSpammer
 }
